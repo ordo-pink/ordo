13e80b09651576624bd13b2f15af25f71374abc6
Sergei Orlov <hello@orlow.dev>
2021-12-19 16:28:50 +0300

    :sparkles: Add react icons

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Scrollbars from "react-custom-scrollbars";
+import { HiOutlineCog, HiOutlineShare, HiOutlineCollection } from "react-icons/hi";
 
 import { Editor } from "./editor/editor";
 
@@ -44,9 +45,15 @@ export const App: React.FC = () => {
 
 				<div className="flex flex-col items-center justify-between p-2 w-12 border-l border-gray-300 bg-gray-200">
 					<div className="flex flex-col h-screen space-y-3">
-						<div className="text-4xl cursor-pointer">📄</div>
-						<div className="text-4xl cursor-pointer">🌲</div>
-						<div className="text-4xl cursor-pointer">⚙️</div>
+						<div className="text-3xl text-pink-500 cursor-pointer">
+							<HiOutlineCollection />
+						</div>
+						<div className="text-3xl text-gray-500 cursor-pointer">
+							<HiOutlineShare />
+						</div>
+						<div className="text-3xl text-gray-500 cursor-pointer">
+							<HiOutlineCog />
+						</div>
 					</div>
 				</div>
 			</div>

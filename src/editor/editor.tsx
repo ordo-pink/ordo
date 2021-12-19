2b6dafe8cf8052798a38d722b436f553b60a0465
Sergei Orlov <hello@orlow.dev>
2021-12-19 18:37:01 +0300

    :recycle: Extract Char component to a separate file

--- src/editor/editor.tsx
+++ src/editor/editor.tsx
@@ -3,110 +3,17 @@ import { CaretPosition, ChangeResponse, ChangeSelection } from "./types";
 import React from "react";
 
 import "./editor.css";
+
 import { useAppDispatch } from "../redux/hooks";
 import { addStatusBarItem, updateStatusBarItem, removeStatusBarItem } from "../status-bar/state";
+
+import { Char } from "./char";
 
 const getStatusLine = (selection: ChangeSelection) => {
 	const line = selection.direction === "rtl" ? selection.start.line : selection.end.line;
 	const index = selection.direction === "rtl" ? selection.start.index : selection.end.index;
 
-	// return JSON.stringify(selection);
 	return `Ln ${line}, Col ${index}`;
-};
-
-const selectionExists = (selection: ChangeSelection) =>
-	selection.start.index !== selection.end.index || selection.start.line !== selection.end.line;
-
-const checkIsInSelection = (selection: ChangeSelection, charIndex: number, lineIndex: number) => {
-	const exists = selectionExists(selection);
-	const betweenSelectedLines = exists && lineIndex > selection.start.line && lineIndex < selection.end.line;
-	const onLastSelectedLine =
-		exists &&
-		selection.start.line !== selection.end.line &&
-		lineIndex === selection.end.line &&
-		charIndex < selection.end.index;
-	const onFirstSelectedLine =
-		exists &&
-		selection.start.line !== selection.end.line &&
-		lineIndex === selection.start.line &&
-		charIndex >= selection.start.index;
-	const onlyOneLineSelected =
-		selection.start.index !== selection.end.index &&
-		selection.start.line === selection.end.line &&
-		lineIndex === selection.start.line &&
-		charIndex >= selection.start.index &&
-		charIndex < selection.end.index;
-
-	return betweenSelectedLines || onLastSelectedLine || onFirstSelectedLine || onlyOneLineSelected;
-};
-
-const Char: React.FC<{
-	value: string;
-	lineIndex: number;
-	charIndex: number;
-	mouseUpHandler: (charIndex: number, lineIndex: number) => void;
-	mouseDownHandler: (charIndex: number, lineIndex: number) => void;
-	selection: ChangeSelection;
-}> = ({ value, charIndex, lineIndex, mouseUpHandler, mouseDownHandler, selection }) => {
-	const ref = React.useRef<HTMLSpanElement>(null);
-
-	const onMouseUp = (e: React.MouseEvent<HTMLSpanElement>) => {
-		e.stopPropagation();
-		e.preventDefault();
-
-		ref.current && ref.current.classList.add("caret");
-
-		mouseUpHandler(charIndex, lineIndex);
-	};
-
-	const onMouseDown = (e: React.MouseEvent<HTMLSpanElement>) => {
-		e.stopPropagation();
-		e.preventDefault();
-
-		mouseDownHandler(charIndex, lineIndex);
-	};
-
-	const isInSelection = checkIsInSelection(selection, charIndex, lineIndex);
-
-	let className = "";
-
-	if (selectionExists && isInSelection) {
-		className += "editor-selection bg-pink-200 ";
-	}
-
-	if (value === "\t") {
-		className += "tab text-pink-500 ";
-	}
-
-	if (value === " ") {
-		className += "space text-pink-500 ";
-	}
-
-	if (value === "\n") {
-		className += "new-line ";
-	}
-
-	return (
-		<span
-			ref={ref}
-			style={{
-				display: "inline-block",
-				whiteSpace: "pre",
-			}}
-			className={className}
-			data-index={`line-${lineIndex}-${charIndex}`}
-			id={`line-${lineIndex}-${charIndex}`}
-			onMouseUp={onMouseUp}
-			onMouseDown={onMouseDown}
-			onMouseOver={(e) => {
-				if (e.buttons === 1) {
-					onMouseUp(e);
-				}
-			}}
-		>
-			{value}
-		</span>
-	);
 };
 
 const ignoredKeyPresses = ["Meta", "Control", "Alt", "Shift", "CapsLock"];
@@ -206,7 +113,7 @@ export const Editor: React.FC = () => {
 		<div>
 			<div
 				ref={ref}
-				className="outline-none text-lg break-words w-full pb-96 cursor-text font-mono tracking-wide"
+				className="outline-none break-words w-full pb-96 cursor-text font-mono tracking-wide"
 				onMouseDown={(e) => {
 					e.preventDefault();
 

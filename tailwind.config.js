407d3d55dbac6edc8b0e3f2bce3e5b24a3ccf909
Sergei Orlov <hello@orlow.dev>
2021-12-19 20:46:41 +0300

    :sparkles: Add support for all TailwindCSS colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require("tailwindcss/colors");
+
 module.exports = {
 	darkMode: "media",
 	content: ["./src/**/*.{ts,tsx,js,jsx,html}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors,
+		},
 	},
 	plugins: [],
 };

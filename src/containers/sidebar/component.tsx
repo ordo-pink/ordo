755ca50d1a722b5daeeb6d6920e2f8647eb636af
Sergei Orlov <hello@orlow.dev>
2022-01-16 19:54:12 +0300

    :art: Remove redundant imports

--- src/containers/sidebar/component.tsx
+++ src/containers/sidebar/component.tsx
@@ -1,5 +1,3 @@
 import React from "react"
-import { NotificationCenter } from "../../components/notification-center/component"
-import { ShareMenu } from "../../components/share/component"
 
 export const Sidebar: React.FC = () => <div></div>

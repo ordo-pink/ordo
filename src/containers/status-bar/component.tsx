75ecfa27a1a75a8769a6e3bb44c8bac2187256e5
Sergei Orlov <hello@orlow.dev>
2022-01-17 17:08:08 +0300

    :bug: Remove notification text in status bar

--- src/containers/status-bar/component.tsx
+++ src/containers/status-bar/component.tsx
@@ -6,7 +6,7 @@ export const StatusBar: React.FC = () => (
 	<div className="flex items-center justify-between">
 		<div></div>
 		<div className="flex">
-			<StatusBarItem icon={HiOutlineShare} text="Notification" show={true} description="Leave Feedback" />
+			<StatusBarItem icon={HiOutlineShare} show={true} description="Leave Feedback" />
 			<StatusBarItem icon={HiOutlineBell} show={true} description="Notifications" />
 		</div>
 	</div>

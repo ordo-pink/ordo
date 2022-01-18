d6c54f1bd0fc41f82bc23f7c9a957e015e250125
Sergei Orlov <hello@orlow.dev>
2022-01-18 20:04:07 +0300

    :recycle: Move ActivityBar commands to activity-bar

---
+++ src/containers/activity-bar/commands.ts
@@ -0,0 +1,17 @@
+export default [
+	{
+		icon: "HiOutlineSwitchHorizontal",
+		name: "Toggle Activity Bar",
+		description: "Shows or hides the activity bar located on the left of the application window.",
+	},
+	{
+		icon: "HiOutlineStatusOnline",
+		name: "Show Activity Bar",
+		description: "Shows the activity bar located on the left of the application window.",
+	},
+	{
+		icon: "HiOutlineStatusOffline",
+		name: "Hide Activity Bar",
+		description: "Hides the activity bar located on the left of the application window.",
+	},
+]
--- src/containers/commander/initial-state.ts
+++ src/containers/commander/initial-state.ts
@@ -2,24 +2,7 @@ import { CommanderState } from "./types"
 
 const initialState: CommanderState = {
 	show: false,
-	items: [
-		// TODO: Move ActivityBar commands to activity-bar
-		{
-			icon: "HiOutlineSwitchHorizontal",
-			name: "Toggle Activity Bar",
-			description: "Shows or hides the activity bar located on the left of the application window.",
-		},
-		{
-			icon: "HiOutlineStatusOnline",
-			name: "Show Activity Bar",
-			description: "Shows the activity bar located on the left of the application window.",
-		},
-		{
-			icon: "HiOutlineStatusOffline",
-			name: "Hide Activity Bar",
-			description: "Hides the activity bar located on the left of the application window.",
-		},
-	],
+	items: [],
 }
 
 export default initialState

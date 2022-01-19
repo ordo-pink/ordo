cd16cee6d1a3ae6abb30d2cc283413c7d5f39673
Sergei Orlov <hello@orlow.dev>
2022-01-19 10:36:28 +0300

    :fire: Remove root level types

--- src/types.ts
+++
@@ -1,7 +0,0 @@
-import { ActivityBarEvent } from "./containers/activity-bar/types"
-import { CommanderEvent } from "./containers/commander/types"
-import { SidebarEvent } from "./containers/sidebar/types"
-import { SuggestionsEvent } from "./containers/suggestions/types"
-import { WorkspaceEvent } from "./containers/workspace/types"
-
-export type OrdoEvents = ActivityBarEvent | CommanderEvent | SidebarEvent | SuggestionsEvent | WorkspaceEvent

b7b7c018016f621464b69b15b05ddf0a56e579ad
Sergei Orlov <hello@orlow.dev>
2022-01-19 18:22:46 +0300

    :label: Properly register ApplicationEvent

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,11 +1,11 @@
 import { BrowserWindow } from "electron"
-import { ApplicationState } from "../application/types"
+import { ApplicationEvent, ApplicationState } from "../application/types"
 import { ActivityBarEvent, ActivityBarState } from "../containers/activity-bar/types"
 import { CommanderEvent, CommanderState } from "../containers/commander/types"
 import { SidebarEvent, SidebarState } from "../containers/sidebar/types"
 import { WorkspaceEvent, WorkspaceState } from "../containers/workspace/types"
 
-export type OrdoEvents = ["@application/get-state"] | ActivityBarEvent | CommanderEvent | SidebarEvent | WorkspaceEvent
+export type OrdoEvents = ApplicationEvent | ActivityBarEvent | CommanderEvent | SidebarEvent | WorkspaceEvent
 
 export type Optional<T> = T | undefined
 

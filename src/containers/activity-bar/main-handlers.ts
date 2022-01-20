56d1855d7e8648af681e3d726e18db3d8e6cc822
Sergei Orlov <hello@orlow.dev>
2022-01-20 18:14:39 +0300

    :bug: Close Commander on changing current activity

--- src/containers/activity-bar/main-handlers.ts
+++ src/containers/activity-bar/main-handlers.ts
@@ -1,3 +1,4 @@
+import { ipcMain } from "electron"
 import { registerIpcMainHandlers } from "../../common/register-ipc-main-handlers"
 import { ActivityBarEvent } from "./types"
 
@@ -10,23 +11,30 @@ export default registerIpcMainHandlers<ActivityBarEvent>({
 		draft.workspace.component = "WelcomePage"
 		draft.commander.show = false
 		draft.sidebar.width = 25
+
+		ipcMain.emit("@commander/hide")
 	},
 	"@activity-bar/open-graph": (draft) => {
 		draft.activities.current = "Graph"
 		draft.workspace.component = "Graph"
-		draft.commander.show = false
 		draft.sidebar.width = 0
+
+		ipcMain.emit("@commander/hide")
 	},
 	"@activity-bar/open-find-in-files": (draft) => {
 		draft.activities.current = "Find in Files"
 		draft.workspace.component = "WelcomePage"
 		draft.commander.show = false
 		draft.sidebar.width = 0
+
+		ipcMain.emit("@commander/hide")
 	},
 	"@activity-bar/open-settings": (draft) => {
 		draft.activities.current = "Settings"
 		draft.workspace.component = "WelcompePage"
 		draft.commander.show = false
 		draft.sidebar.width = 0
+
+		ipcMain.emit("@commander/hide")
 	},
 })

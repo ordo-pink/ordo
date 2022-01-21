2853c0e5cc0531a6cec3a56a97a423cb7fb9b051
Sergei Orlov <hello@orlow.dev>
2022-01-21 14:58:02 +0300

    :sparkles: Add file associations to OrdoFile

--- src/application/create-ordo-file.ts
+++ src/application/create-ordo-file.ts
@@ -12,6 +12,26 @@ const toReadableSize = (a = 0, b = 2, k = 1024): string => {
 	}`
 }
 
+const fileAssociations = [
+	{ extension: ".apng", association: "image" },
+	{ extension: ".avif", association: "image" },
+	{ extension: ".gif", association: "image" },
+	{ extension: ".jpg", association: "image" },
+	{ extension: ".jpeg", association: "image" },
+	{ extension: ".pjpeg", association: "image" },
+	{ extension: ".pjp", association: "image" },
+	{ extension: ".png", association: "image" },
+	{ extension: ".svg", association: "image" },
+	{ extension: ".webp", association: "image" },
+	{ extension: ".bmp", association: "image" },
+	{ extension: ".ico", association: "image" },
+	{ extension: ".cur", association: "image" },
+	{ extension: ".tif", association: "image" },
+	{ extension: ".tiff", association: "image" },
+	{ extension: ".md", association: "text" },
+	{ extension: ".txt", association: "text" },
+]
+
 interface CreateOrdoFileArg {
 	path: string
 	size?: number
@@ -34,7 +54,7 @@ export const createOrdoFile = ({
 	const readableName = path.substring(path.lastIndexOf("/") + 1)
 	const extension = readableName.substring(readableName.lastIndexOf(".")) ?? ".md"
 
-	const type = extension === ".md" ? "text" : "image"
+	const type = fileAssociations.find((assoc) => assoc.extension === extension)?.association || "other"
 
 	const readableSize = toReadableSize(size)
 

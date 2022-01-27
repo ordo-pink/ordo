60d8020071873a8a52f9220ea799c7c50c8d3abb
Sergei Orlov <hello@orlow.dev>
2022-01-27 17:10:39 +0300

    :bug: Fix focusing editor window

--- src/containers/commander/main-handlers.ts
+++ src/containers/commander/main-handlers.ts
@@ -12,13 +12,13 @@ export default registerEventHandlers<CommanderEvent>({
 		transmission.emit("@commander/get-items", "");
 		transmission.emit("@application/set-focused-component", "commander");
 	},
-	"@commander/hide": ({ draft }) => {
+	"@commander/hide": ({ draft, transmission }) => {
 		draft.commander.show = false;
+		transmission.emit("@application/set-focused-component", "editor");
 	},
 	"@commander/run": ({ draft, payload, transmission }) => {
 		draft.commander.show = false;
 		transmission.emit(payload as keyof OrdoEvents);
-		transmission.emit("@application/set-focused-component", "editor");
 	},
 	"@commander/toggle": ({ transmission }) => {
 		transmission.emit(transmission.get((s) => s.commander.show) ? "@commander/hide" : "@commander/show");

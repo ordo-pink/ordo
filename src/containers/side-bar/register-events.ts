0fc5ff00b562165e1b4bf83c43222f3ae5061784
Sergei Orlov <orlowdev@gmail.com>
2022-05-11 19:52:16 +0300

    :recycle: Refactor SideBar container

--- src/containers/side-bar/register-events.ts
+++ src/containers/side-bar/register-events.ts
@@ -1,31 +1,61 @@
+import { Either } from "or-else";
+
+import { OrdoEventHandler } from "@core/types";
 import { registerEvents } from "@core/transmission/register-ordo-events";
-import { SideBarEvents } from "@containers/side-bar/types";
 import { internalSettingsStore } from "@core/settings/internal-settings";
+import { SideBarEvents } from "@containers/side-bar/types";
+import { FoldVoid, fromBoolean } from "@utils/either";
+import { tap } from "@utils/tap";
+
+/**
+ * Triggers hiding the SideBar. This method does not change width of the SideBar so that it can be
+ * reopened with the same width as it was before.
+ */
+const hideHandler: OrdoEventHandler<"@side-bar/hide"> = ({ draft }) => void (draft.sideBar.show = false);
+
+/**
+ * Triggers showing the SideBar. If SideBar width is set to be less than 1% of the screen width,
+ * the width will be updated to take 20% of the screen size.
+ */
+const showHandler: OrdoEventHandler<"@side-bar/show"> = ({ draft, transmission }) =>
+	Either.fromNullable(transmission.select((state) => state.sideBar.width))
+		.chain((width) => fromBoolean(width <= 1))
+		.map(() => (draft.sideBar.width = 20))
+		.bimap(
+			() => (draft.sideBar.show = true),
+			() => (draft.sideBar.show = true),
+		)
+		.fold(...FoldVoid);
+
+/**
+ * Toggles the SideBar. It internally uses `@side-bar/show` and `@side-bar/hide events`, depending on the
+ * current state of the SideBar.
+ */
+const toggleHandler: OrdoEventHandler<"@side-bar/toggle"> = ({ transmission }) =>
+	fromBoolean(transmission.select((state) => state.sideBar.show))
+		.bimap(
+			() => transmission.emit("@side-bar/show", null),
+			() => transmission.emit("@side-bar/hide", null),
+		)
+		.fold(...FoldVoid);
+
+/**
+ * Set width of the SideBar. If the width set is under 1% of the screen, the SideBar is also marked hidden.
+ */
+const setWidthHandler: OrdoEventHandler<"@side-bar/set-width"> = ({ draft, payload }) =>
+	Either.right(payload)
+		.map(tap((p) => (draft.sideBar.width = p)))
+		.map(tap((p) => internalSettingsStore.set("window.sideBarWidth", p)))
+		.chain((p) => fromBoolean(p >= 1))
+		.bimap(
+			() => (draft.sideBar.show = false),
+			() => (draft.sideBar.show = true),
+		)
+		.fold(...FoldVoid);
 
 export default registerEvents<SideBarEvents>({
-	"@side-bar/hide": ({ draft }) => {
-		draft.sideBar.show = false;
-	},
-	"@side-bar/show": ({ draft, transmission }) => {
-		draft.sideBar.show = true;
-
-		const width = transmission.select((state) => state.sideBar.width);
-
-		if (width <= 1) {
-			draft.sideBar.width = 20;
-		}
-	},
-	"@side-bar/toggle": ({ draft, transmission }) => {
-		const { show, width } = transmission.select((state) => state.sideBar);
-
-		draft.sideBar.show = !show;
-
-		if (show && width <= 1) {
-			draft.sideBar.width = 20;
-		}
-	},
-	"@side-bar/set-width": ({ draft, payload }) => {
-		draft.sideBar.width = payload;
-		internalSettingsStore.set("window.sideBarWidth", payload);
-	},
+	"@side-bar/hide": hideHandler,
+	"@side-bar/show": showHandler,
+	"@side-bar/toggle": toggleHandler,
+	"@side-bar/set-width": setWidthHandler,
 });

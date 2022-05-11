293de06633a53166aa5b140aedf00faa3e8a348f
Sergei Orlov <orlowdev@gmail.com>
2022-05-11 16:43:06 +0300

    :recycle: Add some FP to WelcomePage

--- src/modules/welcome-page/components/recent-projects.tsx
+++ src/modules/welcome-page/components/recent-projects.tsx
@@ -2,16 +2,18 @@ import React from "react";
 
 import { useAppSelector } from "@core/state/store";
 import { RecentProject } from "@modules/welcome-page/components/recent-project";
+import { fromBoolean } from "@utils/either";
+import { NoOp } from "@utils/no-op";
 
 export const RecentProjects: React.FC = () => {
 	const recentProjects = useAppSelector((state) => state.app.internalSettings.window?.recentProjects);
 
-	return recentProjects && recentProjects.length > 0 ? (
+	return fromBoolean(recentProjects.length > 0).fold(NoOp, () => (
 		<div>
 			<h2 className="welcome-page_actions-heading">Recent Projects</h2>
 			{recentProjects.map((path) => (
 				<RecentProject key={path} path={path} />
 			))}
 		</div>
-	) : null;
+	));
 };

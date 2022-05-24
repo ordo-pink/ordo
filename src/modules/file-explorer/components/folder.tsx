22801317b40a8a5d510b2502473c6a06950b3307
Sergei Orlov <orlowdev@gmail.com>
2022-05-24 16:46:10 +0300

    :bug: Fix background color freezing when drag'n'dropping in FileExplorer

--- src/modules/file-explorer/components/folder.tsx
+++ src/modules/file-explorer/components/folder.tsx
@@ -10,6 +10,7 @@ import { FolderContent } from "@modules/file-explorer/components/folder-content"
 import { tapPreventDefault, tapStopPropagation } from "@utils/events";
 import { FoldVoid } from "@utils/either";
 import { NoOp } from "@utils/no-op";
+import { tap } from "@utils/functions";
 
 type FolderProps = {
 	folder: OrdoFolder;
@@ -25,6 +26,7 @@ export const Folder: React.FC<FolderProps> = ({ folder }) => {
 	const paddingLeft = useTreeNesting(folder.depth);
 
 	const [iconClassName, setIconClassName] = React.useState<string>("file-explorer_folder_green");
+	const [draggedOver, setDraggedOver] = React.useState<boolean>(false);
 
 	React.useEffect(() => {
 		setIconClassName(`file-explorer_folder_${folder.color}`);
@@ -34,14 +36,14 @@ export const Folder: React.FC<FolderProps> = ({ folder }) => {
 		Either.right(event)
 			.map(tapPreventDefault)
 			.map(tapStopPropagation)
-			.map((e) => e.currentTarget.classList.remove("bg-gray-300"))
+			.map(() => (draggedOver ? setDraggedOver(false) : void 0))
 			.fold(...FoldVoid);
 
 	const handleDragOver = (event: React.MouseEvent) =>
 		Either.right(event)
 			.map(tapPreventDefault)
 			.map(tapStopPropagation)
-			.map((e) => e.currentTarget.classList.add("bg-gray-300"))
+			.map(() => (draggedOver ? void 0 : setDraggedOver(true)))
 			.fold(...FoldVoid);
 
 	const handleDrop = (event: React.DragEvent) =>
@@ -51,6 +53,7 @@ export const Folder: React.FC<FolderProps> = ({ folder }) => {
 			.map((e) => ({ name: e.dataTransfer.getData("fileName"), oldPath: e.dataTransfer.getData("oldPath") }))
 			.map(({ name, oldPath }) => ({ name, oldPath, newFolder: folder.path }))
 			.map((payload) => dispatch({ type: "@file-explorer/move", payload }))
+			.map(() => (draggedOver ? setDraggedOver(false) : void 0))
 			.fold(...FoldVoid);
 
 	const handleDragStart = (event: React.DragEvent) => {
@@ -67,7 +70,13 @@ export const Folder: React.FC<FolderProps> = ({ folder }) => {
 	const handleClick = () => dispatch({ type: "@file-explorer/toggle-folder", payload: folder.path });
 
 	return Either.fromNullable(folder).fold(NoOp, () => (
-		<div onDragLeave={handleDragLeave} onDragOver={handleDragOver} onDrop={handleDrop} title={folder.path}>
+		<div
+			className={draggedOver ? "bg-neutral-400 dark:bg-neutral-800" : ""}
+			onDragLeave={handleDragLeave}
+			onDragOver={handleDragOver}
+			onDrop={handleDrop}
+			title={folder.path}
+		>
 			<div
 				className="file-explorer_item"
 				draggable={true}

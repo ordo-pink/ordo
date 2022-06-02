5211a0a9243d144d38d105b595ed86e021adf70d
Sergei Orlov <orlowdev@gmail.com>
2022-06-02 11:47:07 +0300

    :recycle: Remove separation between empty and non-empty files in the file icon

--- src/modules/file-explorer/hooks/use-file-icon.ts
+++ src/modules/file-explorer/hooks/use-file-icon.ts
@@ -2,14 +2,22 @@ import React from "react";
 
 import { useIcon } from "@core/hooks/use-icon";
 import { OrdoFile } from "@modules/file-explorer/types";
+import { Either, Switch } from "or-else";
+import { id } from "@utils/functions";
 
 export const useFileIcon = (file?: OrdoFile | null) => {
-	const iconName = React.useMemo(() => {
-		if (!file) return;
-		if (file.type === "image") return "HiOutlinePhotograph";
-		if (file.size > 0) return "HiOutlineDocumentText";
-		else return "HiOutlineDocument";
-	}, [file, file && file.path, file && file.type, file && file.size]);
+	const iconName = React.useMemo(
+		() =>
+			Either.fromNullable(file)
+				.map((f) =>
+					Switch.of(f.type)
+						.case("image", "HiOutlinePhotograph" as const)
+						.case("document", "HiOutlineDocument" as const)
+						.default("HiOutlineDocumentSearch" as const),
+				)
+				.fold(() => "HiOutlineDocumentSearch" as const, id),
+		[file, file && file.path, file && file.type, file && file.size],
+	);
 
 	const Icon = useIcon(iconName);
 

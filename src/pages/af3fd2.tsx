741dca02b45ef6d3324a10870a73be5935e7ecf7
Sergei Orlov <orlowdev@gmail.com>
2022-06-15 14:18:20 +0300

    :sparkles: Add warnings

--- src/pages/af3fd2.tsx
+++ src/pages/af3fd2.tsx
@@ -13,7 +13,7 @@ const PrivateBeta = () => {
           Bring Your Thoughts to
         </h2>
         <GradientTitle text="Beta" />
-        <div className="mt-5 flex flex-col space-y-2 w-full">
+        <div className="mt-12 flex flex-col space-y-2 w-full">
           <a
             href="https://tinyurl.com/ordo-0-1-0-mac"
             className="flex space-x-2 justify-center items-center bg-gradient-to-tr from-purple-600 to-pink-700 text-neutral-200 font-bold px-2 py-1 rounded-lg shadow-lg"
@@ -30,6 +30,45 @@ const PrivateBeta = () => {
             <DiWindows />
             <div>Download for Windows</div>
           </a>
+        </div>
+        <div className="mt-12 bg-rose-200 dark:bg-rose-800 w-72 p-2 rounded-lg">
+          <p>
+            <strong>Beware, 🐲 dragons here!</strong>
+          </p>
+          <p>Make a copy of the files you work with. Just in case.</p>
+        </div>
+        <div className="mt-5 bg-rose-200 dark:bg-rose-800 w-72 p-2 rounded-lg">
+          <p>
+            Also, there might be bugs. Chances are, chances are high. It's{" "}
+            <strong>v0.1.0</strong>, afterall. If you find any, drop them on
+            Twitter{" "}
+            <a
+              rel="nofollow"
+              target="_blank"
+              className="underline"
+              href="https://twitter.com/ordo_pink"
+            >
+              @ordo_pink
+            </a>
+            , reach out via email at{" "}
+            <a
+              rel="nofollow"
+              className="underline"
+              href="mailto:hello@ordo.pink"
+            >
+              hello@ordo.pink
+            </a>{" "}
+            or create an issue on{" "}
+            <a
+              rel="nofollow"
+              target="_blank"
+              className="underline"
+              href="https://github.com/ordo-pink/releases/issues"
+            >
+              Github
+            </a>
+            .
+          </p>
         </div>
       </div>
       <Footer />

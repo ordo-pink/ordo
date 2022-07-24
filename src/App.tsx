fd6a6b9c296964b033e5172871c8a8ceb105945c
Kirill Vasiliev <kirillwork56@gmail.com>
2022-07-24 11:05:56 +0300

    ➕ added Internationalization (i18next)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import styles from "./App.module.css";
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1 className={styles.title}>Hello world!</h1>
+      <h1 className={styles.title}>{t("app.example.title")}</h1>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,40 @@
+import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { I18nextProvider, initReactI18next } from "react-i18next";
 import App from "./App";
+import en from "./i18n/en.json";
+import ru from "./i18n/ru.json";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: en,
+      },
+      ru: {
+        translation: ru,
+      },
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+const app = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

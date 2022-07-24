27755313f841692ec8876dd962d58decef41116b
Timur Gafiulin <t.gafiulin@ya.ru>
2022-07-24 21:17:31 +0300

    :wrench: add eslintrc
    move eslintconfig from package.json to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    extends: [
+        "react-app",
+        "react-app/jest",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript"
+    ],
+    parser: "@typescript-eslint/parser",
+    settings: {
+        "import/resolver": {
+            "typescript": {}
+        }
+    }
+}

ab54a439727dfa6f873dc2a4c898353fd8607a71
Kirill Vasiliev <kirillwork56@gmail.com>
2022-07-29 22:18:57 +0300

    :heavy_plus_sign: add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,31 @@
 module.exports = {
-    env: {
-        "browser": true,
-        "es6": true,
-        "node": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "react-app",
+    "react-app/jest",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:storybook/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  settings: {
+    "import/resolver": {
+      typescript: {},
     },
-    extends: [
-        "react-app",
-        "react-app/jest",
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript"
-    ],
-    parser: "@typescript-eslint/parser",
-    settings: {
-        "import/resolver": {
-            "typescript": {}
-        }
-    }
-}
+  },
+  overrides: [
+    {
+      files: ["**/*.stories.*"],
+      rules: {
+        "import/no-anonymous-default-export": "off",
+      },
+    },
+  ],
+};

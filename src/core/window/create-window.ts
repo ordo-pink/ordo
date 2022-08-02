933528482892446e03ae9cc4ee9f78c38af44c14
Sergei Orlov <orlowdev@gmail.com>
2022-08-02 20:02:08 +0300

    :lipstick: Update app icon

--- src/core/window/create-window.ts
+++ src/core/window/create-window.ts
@@ -33,7 +33,7 @@ export const createWindow = async (): Promise<void> => {
 		width: internalSettingsStore.get("window.width"),
 		x: internalSettingsStore.get("window.position.x"),
 		y: internalSettingsStore.get("window.position.y"),
-		icon: "assets/icons/png/1024x1024.png",
+		icon: "assets/android-chrome-512x512.png",
 		// titleBarStyle: "hiddenInset",
 		acceptFirstMouse: true,
 		webPreferences: {

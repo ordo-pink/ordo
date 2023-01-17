fdd185a0c679df447accb80673c8414c6bea4098
Andrey Orlov <krotocat@gmail.com>
2023-01-17 16:37:50 +0700

    :bug: Create root directory if it doesnt exist on get

--- src/fs/driver/methods/get-directory.ts
+++ src/fs/driver/methods/get-directory.ts
@@ -2,15 +2,24 @@ import { promises } from "fs"
 
 import { Either } from "$core/either"
 import { FSDriver, OrdoDirectory } from "$core/types"
+import { OrdoDirectoryPath } from "$core/types"
 
 import { Exception } from "$fs/constants"
 import { getNormalizedAbsolutePath } from "$fs/driver/utils/get-normalized-absolute-path"
 import { listDirectory } from "$fs/driver/utils/list-directory"
+import { createDirectory } from "./create-directory"
 
 export const getDirectory =
   (directory: string): FSDriver["getDirectory"] =>
   async (path) => {
     const absolutePath = getNormalizedAbsolutePath(path, directory)
+    if (path === ("" as OrdoDirectoryPath)) {
+      const stat = await promises.stat(absolutePath).catch(() => null)
+
+      if (!stat || !stat.isDirectory()) {
+        await createDirectory(directory)("" as OrdoDirectoryPath)
+      }
+    }
 
     const stat = await promises.stat(absolutePath).catch(() => null)
 

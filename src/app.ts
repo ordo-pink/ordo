e89fe83c9f3261d4efa69b3d47dcc0d335d047ef
Sergei Orlov <orlowdev@gmail.com>
2023-01-18 13:33:03 +0300

    :sparkles: Add support for CORS

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { urlencoded } from "body-parser"
 import compression from "compression"
+import cors from "cors"
 import express, { Request } from "express"
 
 import { Drivers } from "$core/types"
@@ -12,6 +13,7 @@ const filterCompression = (req: Request) => !req.headers["x-no-compression"]
 
 export const createOrdoBackendServer = (drivers: Drivers) =>
   app
+    .use(cors())
     .use(urlencoded({ extended: false }))
     .use(compression({ filter: filterCompression }))
     .use("/fs", fsRouter(drivers))

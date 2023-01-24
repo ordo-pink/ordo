c6aa453cc8c57bacb494350d7616136320b01b2d
Sergei Orlov <orlowdev@gmail.com>
2023-01-24 18:59:44 +0300

    :bug: Sort directory when appending an item

--- src/containers/app/store/reducers/created.ts
+++ src/containers/app/store/reducers/created.ts
@@ -4,6 +4,7 @@ import { getItemParent } from "$containers/app/store/reducers/common"
 import { AppState } from "$containers/app/types"
 import { OrdoDirectory, OrdoFile } from "$core/types"
 import { Either } from "$core/utils/either"
+import { sortOrdoDirectory } from "$core/utils/fs-helpers"
 import { noOp } from "$core/utils/no-op"
 
 export const createdReducer: CaseReducer<AppState, PayloadAction<OrdoFile | OrdoDirectory>> = (
@@ -12,5 +13,9 @@ export const createdReducer: CaseReducer<AppState, PayloadAction<OrdoFile | Ordo
 ) => {
   Either.fromNullable(payload)
     .chain(getItemParent(state.personalProject))
-    .fold(noOp, (parent) => parent.children.push(payload))
+    .fold(noOp, (parent) => {
+      parent.children.push(payload)
+
+      sortOrdoDirectory(parent)
+    })
 }
--- src/core/utils/fs-helpers.ts
+++ src/core/utils/fs-helpers.ts
@@ -45,3 +45,27 @@ export const findOrdoFile = (
 
   return found
 }
+
+export const sortOrdoDirectory = (directory: OrdoDirectory) => {
+  directory.children = directory.children.sort((a, b) => {
+    if (isOrdoDirectory(a)) {
+      sortOrdoDirectory(a)
+    }
+
+    if (isOrdoDirectory(b)) {
+      sortOrdoDirectory(b)
+    }
+
+    if (!isOrdoDirectory(a) && isOrdoDirectory(b)) {
+      return 1
+    }
+
+    if (isOrdoDirectory(a) && !isOrdoDirectory(b)) {
+      return -1
+    }
+
+    return a.readableName.localeCompare(b.readableName)
+  })
+
+  return directory
+}

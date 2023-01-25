2e15b38de019118cee4fd3a1aac756763b0c699c
Sergei Orlov <orlowdev@gmail.com>
2023-01-25 14:29:18 +0300

    :sparkles: Update file not selected page

--- src/activities/editor/components/file-not-selected.tsx
+++ src/activities/editor/components/file-not-selected.tsx
@@ -1,9 +1,21 @@
 import { useTranslation } from "react-i18next"
+import EditorPage from "./editor-page"
+import NotSelected from "$assets/img/not-selected-3.png"
 
 export default function FileNotSelected() {
   const { t } = useTranslation()
 
   const translatedText = t("@ordo-activity-editor/no-file")
 
-  return <div className="editor_not-selected">{translatedText}</div>
+  return (
+    <EditorPage
+      currentFile={null}
+      image={NotSelected}
+      imageAlt=""
+    >
+      <div className="text-4xl w-full flex flex-col items-center justify-center">
+        {translatedText}
+      </div>
+    </EditorPage>
+  )
 }

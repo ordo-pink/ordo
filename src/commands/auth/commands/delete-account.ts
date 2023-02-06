6652333ecdd69155c7207dec079ff58aaac37a09
Andrey Orlov <krotocat@gmail.com>
2023-02-06 22:38:57 +0700

    :globe_with_meridians: Fix translations

--- src/commands/auth/commands/delete-account.ts
+++ src/commands/auth/commands/delete-account.ts
@@ -2,17 +2,17 @@ import { createOrdoCommand } from "$core/extensions/create-ordo-command"
 
 export const DeleteAccount = createOrdoCommand({
   title: "@ordo-command-auth/delete-account",
-  action: ({ env, translate }) => {
+  action: ({ env, userData, translate }) => {
     const translatedSubject = translate("@ordo-command-auth/delete-account-subject", {
-      email: env.userData?.email,
+      email: userData?.email,
     })
 
     const translatedBody = translate("@ordo-command-auth/delete-account-body", {
-      username: env.userData?.username,
+      username: userData?.username,
     })
 
     env.openExternal(`mailto:support@ordo.pink?subject=${translatedSubject}&body=${translatedBody}`)
   },
-  showInCommandPalette: ({ env }) => Boolean(env.isAuthenticated),
+  showInCommandPalette: ({ isAuthenticated }) => isAuthenticated,
   Icon: () => import("$commands/auth/components/delete-account-icon"),
 })

aa3f4744a71e1be1d839f8039e67c409d26ebd4d
Andrey Orlov <krotocat@gmail.com>
2023-02-13 22:41:20 +0700

    :bug: Fix extension 404 errors

--- src/internal/extensions/handlers/get.ts
+++ src/internal/extensions/handlers/get.ts
@@ -1,4 +1,5 @@
-import { ExceptionResponse, SystemDirectory } from "@ordo-pink/core"
+import { Readable } from "stream"
+import { ExceptionResponse, SuccessResponse, SystemDirectory } from "@ordo-pink/core"
 import { Switch } from "@ordo-pink/switch"
 import { contentType } from "mime-types"
 import { FsRequestHandler } from "../../../fs"
@@ -6,7 +7,7 @@ import { USER_ID_PARAM } from "../../../fs/constants"
 import { ExtensionsParams } from "../types"
 
 export const getExtensionFileHandler: FsRequestHandler<ExtensionsParams> =
-  ({ file: { getFileContent } }) =>
+  ({ file: { getFileContent, createFile } }) =>
   (req, res) => {
     const userId = req.params[USER_ID_PARAM]
     const path = `/${userId}${SystemDirectory.EXTENSIONS}${req.params.extension}.json` as const
@@ -17,7 +18,15 @@ export const getExtensionFileHandler: FsRequestHandler<ExtensionsParams> =
       .then((content) => content.pipe(res))
       .catch((error: ExceptionResponse.NOT_FOUND | Error) =>
         Switch.of(error)
-          .case(ExceptionResponse.NOT_FOUND, () => res.status(ExceptionResponse.NOT_FOUND).send())
+          .case(ExceptionResponse.NOT_FOUND, async () => {
+            const content = new Readable()
+            content.push("{}")
+            content.push(null)
+
+            await createFile({ path, content })
+
+            res.status(SuccessResponse.OK).send("{}")
+          })
           .default(() => {
             req.params.logger.error(error)
             res.status(ExceptionResponse.UNKNOWN_ERROR).send(error.toString())

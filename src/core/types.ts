55231cc3d9c753e3bdb8a961fc1dca75464ff7a2
Sergei Orlov <sergei_orlov@ordo.pink>
2023-02-16 20:32:23 +0300

    :sparkles: Save language in settings to persisted store

--- src/core/types.ts
+++ src/core/types.ts
@@ -19,7 +19,7 @@ import type { RootState } from "$core/state/types"
 
 export type OrdoExtensionPersistedStore<T extends Record<string, unknown>> = {
   init: ThunkFn<Promise<void>>
-  get<K extends keyof T>(key: K): Promise<Nullable<T[K]>>
+  get<K extends keyof T>(key: K): Promise<T[K]>
   set<K extends keyof T>(key: K, value: T[K]): Promise<void>
   clear: ThunkFn<Promise<void>>
   resetDefaults: ThunkFn<Promise<void>>

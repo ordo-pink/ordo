4ecb43df975ca29a66a2480bf68d86145074ff6d
Sergei Orlov <sergei_orlov@ordo.pink>
2023-02-18 23:57:32 +0300

    :white_check_mark: chore: Add API tests and todos for directories

--- .github/contributing.md
+++ .github/contributing.md
@@ -2,15 +2,57 @@
 
 TBD
 
+## Committing
+
+### TL;DR
+
+1. Add code, `git add` it
+2. Run `git commit` without any message. Gitmoji CLI will pop up automatically unless you ignore git
+   hooks.
+
+```sh
+git commit
+```
+
+3. Choose emoji via the picker
+4. Add conventional-commits type (`feat:`, `fix:`, `chore:` and all that)
+5. Add commit message
+6. All done! :sparkles:
+
+Should be something like this in the end:
+
+`:sparkles: feat: Add awesomeness`
+
 ## Commit conventions
 
-`ordo-electron` uses gitmoji commit convention.
+`ordo-electron` uses [gitmoji](https://gitmoji.dev) commit convention paired with
+[conventional commits](https://www.conventionalcommits.org/en/v1.0.0/).
 
-- Every commit MUST start with a type represented by one of commit types listed on [gitmoji.dev](https://gitmoji.dev/). Do not use emoji icons - only code values (e.g. :art :, :sparkles :, etc.)
-- Every commit MUST have a defined scope after the commit type, separated with a space (` `). Commit scope MUST be followed with a colon (`:`) and a space (` `). The scope MUST describe the area that the commit makes changes to (e.g. `activity bar: `, `editor: `, `readme: `, `electron.js: `, etc.)
+- Every commit MUST start with a type represented by one of commit types listed on
+  [gitmoji.dev](https://gitmoji.dev/). Do not use emoji icons - only code values (e.g. :art :,
+  :sparkles :, etc.)
+- Every commit MUST have a defined conventional commits type after the emoji commit type, separated
+  with a space (` `). Conventional commits type MUST be followed with a colon (`:`) and a space
+  (` `). The conventional commits type MUST describe the type of changes to (e.g. `build: `,
+  `feat: `, `fix: `, `chore: `, etc.). The list of conventional commits types is defined below
 - Every commit MUST have a short description of the commit content after the commit scope
 
-### Most common commit types
+### Conventional-commits types
+
+- build
+- ci
+- docs
+- feat
+- fix
+- perf
+- refactor
+- revert
+- style
+- test
+- chore
+- wip
+
+### Most common emoji commit types
 
 - :sparkles : - adding new feature
 - :bug : - fixing a bug
--- apps/backend-local-e2e/src/backend-local/backend-local.spec.ts
+++ apps/backend-local-e2e/src/backend-local/backend-local.spec.ts
@@ -39,6 +39,9 @@ describe("directories", () => {
         })
       }))
 
+    it.todo("should return 400 if requested path targets at internal files")
+    it.todo("should return 400 if requested path is not valid")
+
     it("should return 409 if a directory already exists", () =>
       axios.post(`/fs/directories/${token}/e2e/dir1/`, { headers }).then(() =>
         axios.post(`/fs/directories/${token}/e2e/dir1/`, { headers }).catch((error: AxiosError) => {
@@ -75,6 +78,9 @@ describe("directories", () => {
         ),
       ))
 
+    it.todo("should return 400 if requested path targets at internal files")
+    it.todo("should return 400 if requested path is not valid")
+
     it("should return 404 if a directory does not exist", () =>
       axios.post(`/fs/directories/${token}/e2e/dir1/`, { headers }).then(() =>
         axios
@@ -84,6 +90,35 @@ describe("directories", () => {
             expect(error.response.data).toEqual("")
           }),
       ))
+  })
+
+  describe("get-directory", () => {
+    it("should get a directory", () =>
+      axios
+        .post(`/fs/directories/${token}/e2e/`, { headers })
+        .then(() => axios.get(`/fs/directories/${token}/e2e/`))
+        .then((res) => {
+          expect(res.status).toEqual(200)
+          expect(res.data).toEqual({
+            path: "/e2e/",
+            children: [],
+          })
+        }))
+
+    it.todo("should return the root directory on /")
+    it.todo("should get a directory with nested content")
+    it.todo("should return 400 if requested path targets at internal files")
+    it.todo("should return 400 if requested path is not valid")
+    it.todo("should return 404 if the folder does not exist")
+  })
+
+  describe("move-directory", () => {
+    it.todo("should move a directory")
+    it.todo("should move a directory and create parent directories")
+    it.todo("should return 400 if requested path targets at internal files")
+    it.todo("should return 400 if requested path is not valid")
+    it.todo("should return 404 if the old path does not exist")
+    it.todo("should return 409 if the new path already exists")
   })
 
   afterEach(() => axios.delete(`/fs/directories/${token}/e2e/`, { headers }))

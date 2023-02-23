6953510b550426d8e30de7b512f5d71e561f64af
Sergei Orlov <sergei_orlov@ordo.pink>
2023-02-24 01:50:50 +0300

    :bug: fix: Loading screen positioning

--- packages/react/src/lib/loading/loading.tsx
+++ packages/react/src/lib/loading/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "./loader"
 
 export const Loading = () => {
   return (
-    <div className="flex max-h-screen items-center justify-center bg-neutral-50 dark:bg-neutral-700">
+    <div className="flex h-screen items-center justify-center bg-neutral-50 dark:bg-neutral-700">
       <Loader />
     </div>
   )

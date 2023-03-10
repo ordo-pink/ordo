6238597c132045fc21b3ccee9109083aa11e0706
Konstantin Tsepelev <strangeratthedoor@gmail.com>
2023-03-11 02:21:28 +0300

    :art: chore(crypto): extrace max_buf_size to the static

--- apps/crypto/src/main.rs
+++ apps/crypto/src/main.rs
@@ -1,21 +1,22 @@
 use axum::{
-    body::{boxed},
-    extract::{BodyStream},
-    http::{StatusCode},
+    body::boxed,
+    extract::BodyStream,
+    http::StatusCode,
     response::Response,
     routing::{on, MethodFilter},
     Router,
 };
 use axum_extra::body::AsyncReadBody;
-use futures::{StreamExt};
-use std::{net::SocketAddr};
-use tokio::io::{AsyncWriteExt};
+use futures::StreamExt;
+use std::net::SocketAddr;
+use tokio::io::AsyncWriteExt;
 use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
+static MAX_BUF_SIZE: usize = 510241024;
+
 #[tokio::main]
 async fn main() {
-
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -35,12 +36,14 @@ async fn main() {
 }
 
 async fn return_the_same(mut stream: BodyStream) -> Result<Response, StatusCode> {
-    let (rx, tx) = tokio::io::duplex(65_536);
+    let (rx, tx) = tokio::io::duplex(MAX_BUF_SIZE);
     futures::pin_mut!(tx);
     let body = AsyncReadBody::new(rx);
 
     while let Some(chunk) = stream.next().await {
-        tx.write_all(&chunk.unwrap()).await.expect("Dont able to write into stream ;(");
+        tx.write_all(&chunk.unwrap())
+            .await
+            .expect("Dont able to write into stream ;(");
     }
 
     Ok(Response::builder().body(boxed(body)).unwrap())

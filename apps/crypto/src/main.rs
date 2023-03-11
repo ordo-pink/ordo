44334e8bd4124ceaa799adba362f0fcd5c1414b3
Konstantin Tsepelev <strangeratthedoor@gmail.com>
2023-03-12 00:20:26 +0300

    :sparkles: feat(crypto): added encryption

--- apps/crypto/src/main.rs
+++ apps/crypto/src/main.rs
@@ -1,11 +1,14 @@
 use std::net::SocketAddr;
 
+use chacha20poly1305::aead::{OsRng};
+use chacha20poly1305::{aead::Aead, KeyInit, XChaCha20Poly1305};
 use http_body_util::{BodyExt, Full};
-use hyper::body::Frame;
+use hyper::body::{Body, Frame};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{body::Bytes, Method};
 use hyper::{body::Incoming as IncomingBody, Request, Response, StatusCode};
+use rand::RngCore;
 use tokio::net::TcpListener;
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
@@ -18,13 +21,18 @@ fn full<T: Into<Bytes>>(chunk: T) -> BoxBody {
         .boxed()
 }
 
-// static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
 static NOTFOUND: &[u8] = b"Not Found";
 
-async fn register_routes(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
+async fn register_routes(
+    req: Request<IncomingBody>,
+    key: [u8; 32],
+    nonce: [u8; 24],
+) -> Result<Response<BoxBody>> {
     match (req.method(), req.uri().path()) {
         (&Method::POST, "/return_the_same") => return_the_same(req).await,
         (&Method::POST, "/return_the_same/mapped") => return_the_same_mapped(req).await,
+        (&Method::POST, "/encrypt") => enc(req, key, nonce).await,
+        (&Method::POST, "/decrypt") => dec(req, key, nonce).await,
         _ => {
             // Return 404 not found response.
             Ok(Response::builder()
@@ -32,6 +40,70 @@ async fn register_routes(req: Request<IncomingBody>) -> Result<Response<BoxBody>
                 .body(full(NOTFOUND))
                 .unwrap())
         }
+    }
+}
+
+fn encrypt(bytes: &Bytes, key: &[u8; 32], nonce: &[u8; 24]) -> Result<Bytes> {
+    let cipher = XChaCha20Poly1305::new(key.into());
+    Ok(Bytes::from_iter(
+        cipher.encrypt(nonce.into(), bytes.as_ref()).unwrap(),
+    ))
+}
+
+fn decrypt(bytes: &Bytes, key: &[u8; 32], nonce: &[u8; 24]) -> Result<Bytes> {
+    let cipher = XChaCha20Poly1305::new(key.into());
+    Ok(Bytes::from_iter(
+        cipher.decrypt(nonce.into(), bytes.as_ref()).unwrap(),
+    ))
+}
+
+async fn enc(
+    req: Request<IncomingBody>,
+    key: [u8; 32],
+    nonce: [u8; 24],
+) -> Result<Response<BoxBody>> {
+    if req.body().size_hint().upper().unwrap_or(u64::MAX) <= 64 * 1024 {
+        let body = req.collect().await.unwrap_or_default();
+        let body = encrypt(&body.to_bytes(), &key, &nonce).unwrap();
+        Ok(Response::new(full(body)))
+    } else {
+        let frame_stream = req.into_body().map_frame(move |frame| {
+            let frame = if let Ok(data) = frame.into_data() {
+                println!("enc {:?}", data.len());
+                println!("enc {:?}", data.chunks(16).len());
+                encrypt(&data, &key, &nonce).unwrap()
+            } else {
+                Bytes::new()
+            };
+            Frame::data(frame)
+        });
+
+        Ok(Response::new(frame_stream.boxed()))
+    }
+}
+
+async fn dec(
+    req: Request<IncomingBody>,
+    key: [u8; 32],
+    nonce: [u8; 24],
+) -> Result<Response<BoxBody>> {
+  println!("key: {:?} nonce {:?}", key, nonce);
+    if req.body().size_hint().upper().unwrap_or(u64::MAX) > 64 * 1024 {
+        let body = req.collect().await.unwrap_or_default();
+        let body = decrypt(&body.to_bytes(), &key, &nonce).unwrap();
+        Ok(Response::new(full(body)))
+    } else {
+        let frame_stream = req.into_body().map_frame(move |frame| {
+            let frame = if let Ok(data) = frame.into_data() {
+                println!("dec {:?}", data.len());
+                decrypt(&data, &key, &nonce).unwrap()
+            } else {
+                Bytes::new()
+            };
+            Frame::data(frame)
+        });
+
+        Ok(Response::new(frame_stream.boxed()))
     }
 }
 
@@ -43,9 +115,7 @@ async fn return_the_same_mapped(req: Request<IncomingBody>) -> Result<Response<B
     let frame_stream = req.into_body().map_frame(|frame| {
         let frame = if let Ok(data) = frame.into_data() {
             // Convert every byte in every Data frame to uppercase
-            data.iter()
-                .map(|byte| byte.to_be())
-                .collect::<Bytes>()
+            data.iter().map(|byte| byte.to_be()).collect::<Bytes>()
         } else {
             Bytes::new()
         };
@@ -60,15 +130,25 @@ async fn return_the_same_mapped(req: Request<IncomingBody>) -> Result<Response<B
 async fn main() -> Result<()> {
     let addr: SocketAddr = "127.0.0.1:1337".parse().unwrap();
 
+    let mut key = [0u8; 32];
+    let mut nonce = [0u8; 24];
+    OsRng.fill_bytes(&mut key);
+    OsRng.fill_bytes(&mut nonce);
+
     let listener = TcpListener::bind(&addr).await?;
     println!("Listening on http://{}", addr);
     loop {
         let (stream, _) = listener.accept().await?;
 
         tokio::task::spawn(async move {
-            let service = service_fn(move |req| register_routes(req));
+            let key = key.clone();
+            let nonce = nonce.clone();
+
+            println!("key: {:?} nonce {:?}", key, nonce);
+            let service = service_fn(move |req| register_routes(req, key, nonce));
 
             if let Err(err) = http1::Builder::new()
+                .max_buf_size(8 * 1024)
                 .serve_connection(stream, service)
                 .await
             {

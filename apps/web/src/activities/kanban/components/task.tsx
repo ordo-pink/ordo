33a7c3461f21cf47e123a9ce1889246780fe61d9
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2023-03-15 14:40:23 +0300

    :sparkles: feat: Add open file card button

--- apps/web/src/activities/kanban/components/task.tsx
+++ apps/web/src/activities/kanban/components/task.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/require-render-return */
 import { IOrdoFile } from "@ordo-pink/fs-entity"
+import { OrdoButtonSecondary } from "@ordo-pink/react"
 import { Draggable } from "react-beautiful-dnd"
+import { BsArrowsFullscreen } from "react-icons/bs"
+import { createSearchParams, useNavigate } from "react-router-dom"
 
 type Props = {
   task: IOrdoFile
@@ -8,6 +11,8 @@ type Props = {
 }
 
 export default function Task({ task, index }: Props) {
+  const navigate = useNavigate()
+
   return (
     <Draggable
       draggableId={task.path}
@@ -15,7 +20,7 @@ export default function Task({ task, index }: Props) {
     >
       {(provided, snapshot) => (
         <div
-          className={`transition-all duration-300 flex items-center justify-between rounded-lg p-2 shadow-sm ${
+          className={`transition-all duration-300 flex flex-col space-y-4 rounded-lg p-2 shadow-sm ${
             snapshot.isDragging
               ? "bg-gradient-to-tr from-sky-200 via-slate-200 to-pink-200"
               : "bg-neutral-100"
@@ -25,6 +30,19 @@ export default function Task({ task, index }: Props) {
           ref={provided.innerRef}
         >
           <div>{task.readableName}</div>
+          <div className="flex space-x-2 justify-end">
+            <OrdoButtonSecondary
+              compact
+              onClick={() =>
+                navigate({
+                  pathname: "/editor",
+                  search: createSearchParams({ path: task.path }).toString(),
+                })
+              }
+            >
+              <BsArrowsFullscreen />
+            </OrdoButtonSecondary>
+          </div>
         </div>
       )}
     </Draggable>

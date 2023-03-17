90eefa0fc1b20dbea4641cdb9044026e1ecc9792
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2023-03-17 17:18:05 +0300

    :sparkles: feat(kanban): Provide access to Kanban container
    Allow accessing files containing Kanban boards via Kanban Activity sidebar

--- apps/web/src/activities/kanban/components/index.tsx
+++ apps/web/src/activities/kanban/components/index.tsx
@@ -1,9 +1,10 @@
 import { Nullable } from "@ordo-pink/common-types"
 import { Either } from "@ordo-pink/either"
 import { IOrdoFile, OrdoDirectoryPath } from "@ordo-pink/fs-entity"
-import { ActionListItem, Null, useWorkspaceWithSidebar } from "@ordo-pink/react"
+import { Null, useWorkspaceWithSidebar } from "@ordo-pink/react"
 import { useEffect, useState } from "react"
 import Kanban from "./kanban"
+import KanbanFileReference from "./kanban-file-reference"
 import { useAppSelector } from "../../../core/state/hooks/use-app-selector"
 import { getFiles } from "../../../core/utils/fs-helpers"
 
@@ -12,16 +13,31 @@ export default function KanbanActivity() {
 
   const tree = useAppSelector((state) => state.app.personalProject)
 
-  const [kanbanFiles, setKanbanFiles] = useState<IOrdoFile[]>([])
-  const [currentKanbanDirectory, setCurrentKanbanDirectory] =
-    useState<Nullable<OrdoDirectoryPath>>(null)
+  const [kanbans, setKanbans] = useState(
+    {} as Record<OrdoDirectoryPath, IOrdoFile<{ kanbans: OrdoDirectoryPath[] }>[]>,
+  )
+  const [currentKanban, setCurrentKanban] = useState<Nullable<OrdoDirectoryPath>>(null)
 
   useEffect(() => {
     if (!tree) return
 
-    const files = getFiles(tree)
+    const files = getFiles(tree) as IOrdoFile<{ kanbans: OrdoDirectoryPath[] }>[]
 
-    setKanbanFiles(files.filter((file) => Boolean(file.metadata.kanbans)))
+    setKanbans(
+      files
+        .filter((file) => Boolean(file.metadata.kanbans))
+        .reduce((acc, file) => {
+          file.metadata.kanbans.forEach((kanban) => {
+            if (!acc[kanban]) {
+              acc[kanban] = []
+            }
+
+            acc[kanban].push(file)
+          })
+
+          return acc
+        }, {} as Record<OrdoDirectoryPath, IOrdoFile<{ kanbans: OrdoDirectoryPath[] }>[]>),
+    )
   }, [tree])
 
   // TODO: Move kanban selection to URL
@@ -29,21 +45,19 @@ export default function KanbanActivity() {
     <Workspace
       sidebarChildren={
         <div className="p-2">
-          {kanbanFiles.map((kanbanFile) =>
-            (kanbanFile.metadata.kanbans as OrdoDirectoryPath[]).map((directory, index) => (
-              <ActionListItem
-                key={kanbanFile.path}
-                Icon={() => null}
-                isCurrent={directory === currentKanbanDirectory}
-                text={`${kanbanFile.readableName} ${index + 1}`}
-                onClick={() => setCurrentKanbanDirectory(directory)}
-              />
-            )),
-          )}
+          {Object.keys(kanbans).map((kanban) => (
+            <KanbanFileReference
+              key={kanban}
+              isCurrent={kanban === currentKanban}
+              kanban={kanban as OrdoDirectoryPath}
+              setCurrentKanban={setCurrentKanban}
+              files={kanbans[kanban as OrdoDirectoryPath]}
+            />
+          ))}
         </div>
       }
     >
-      {Either.fromNullable(currentKanbanDirectory).fold(Null, (path) => (
+      {Either.fromNullable(currentKanban).fold(Null, (path) => (
         <div className="h-[calc(100vh-10rem)]">
           <Kanban directoryPath={path} />
         </div>

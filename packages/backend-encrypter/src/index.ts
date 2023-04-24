360850fcfcd753fdfc512be5340e6ea11abbd7d2
Sergei Orlov <sergei_orlov@ordo.pink>
2023-04-24 15:09:28 +0300

    :recycle: chore(backend-encrypter): Refactor package

--- packages/backend-encrypter/src/index.ts
+++ packages/backend-encrypter/src/index.ts
@@ -1,60 +1,112 @@
 import { randomBytes, createCipheriv, createDecipheriv, CipherKey, createHash } from "crypto"
-import { Readable, Transform, Writable } from "stream"
+import { Transform, TransformOptions, Writable } from "stream"
+import { DecryptFn, Encrypter, EncryptFn, UnaryFn } from "@ordo-pink/common-types"
+
+// Public ---------------------------------------------------------------------
+
+// Types ----------------------------------------------------------------------
+
+export type Params = {
+  /**
+   * Algorithm to be used for encryption.
+   *
+   * @optional
+   * @default "aes-256-cbc"
+   */
+  algorithm: string
+
+  /**
+   * Cipher key to be used for encryption and decryption.
+   *
+   * @required
+   */
+  encryptionKey: CipherKey
+
+  /**
+   * Initialisation vector size.
+   *
+   * @optional
+   * @default 16
+   */
+  ivSize: number
+
+  /**
+   * Transform stream options.
+   *
+   * @optional
+   * @default undefined
+   */
+  options?: TransformOptions
+}
+
+// Impl -----------------------------------------------------------------------
 
 /**
- * Encryption module that applies aes-256-cbc encryption and decryption on the
- * stream of data.
+ * Creates encryption module that applies aes-256-cbc encryption and decryption
+ * on the stream of data.
  */
 export const createEncryptionModule = (key: string) => {
   const hash = createHash("sha256")
+
   hash.update(key)
+
   const digest = hash.digest()
   const transform = encrypter("aes-256-cbc")
 
   return transform(digest)
 }
 
-const encrypter =
-  (algorythm: string, options?: unknown) =>
-  (encryptionKey: CipherKey, ivSize = 16) => ({
-    encryptStream: (input: Readable) => {
-      const iv = randomBytes(ivSize)
-      const cipher = createCipheriv(algorythm, encryptionKey, iv, options)
+// Internal -------------------------------------------------------------------
 
-      let initialised = false
+const encryptStream: UnaryFn<Params, EncryptFn> =
+  ({ algorithm, encryptionKey, ivSize, options }) =>
+  (input) => {
+    const iv = randomBytes(ivSize)
+    const cipher = createCipheriv(algorithm, encryptionKey, iv, options)
 
-      return input.pipe(cipher).pipe(
-        new Transform({
-          transform(chunk, _, callback) {
-            if (!initialised) {
-              initialised = true
-              this.push(Buffer.concat([iv, chunk]))
-            } else {
-              this.push(chunk)
-            }
+    let initialised = false
 
-            callback()
-          },
-        }),
-      )
-    },
-    decryptStream: (output: Writable) => {
-      let iv: string
-
-      return new Transform({
+    return input.pipe(cipher).pipe(
+      new Transform({
         transform(chunk, _, callback) {
-          if (!iv) {
-            iv = chunk.slice(0, ivSize)
-            const cipher = createDecipheriv(algorythm, encryptionKey, iv)
-
-            this.pipe(cipher).pipe(output)
-            this.push(chunk.slice(ivSize))
+          if (!initialised) {
+            initialised = true
+            this.push(Buffer.concat([iv, chunk]))
           } else {
             this.push(chunk)
           }
 
           callback()
         },
-      })
-    },
+      }),
+    )
+  }
+
+const decryptStream: UnaryFn<Params, DecryptFn> =
+  ({ algorithm, encryptionKey, ivSize }) =>
+  (output: Writable) => {
+    let iv: string
+
+    return new Transform({
+      transform(chunk, _, callback) {
+        if (!iv) {
+          iv = chunk.slice(0, ivSize)
+          const cipher = createDecipheriv(algorithm, encryptionKey, iv)
+
+          this.pipe(cipher).pipe(output)
+          this.push(chunk.slice(ivSize))
+        } else {
+          this.push(chunk)
+        }
+
+        callback()
+      },
+    })
+  }
+
+const encrypter =
+  (algorithm: string, options?: TransformOptions) =>
+  (encryptionKey: CipherKey, ivSize = 16): Encrypter => ({
+    encryptStream: encryptStream({ algorithm, encryptionKey, ivSize, options }),
+    decryptStream: decryptStream({ algorithm, encryptionKey, ivSize, options }),
   })
---
+++ packages/common-types/src/lib/encrypt.ts
@@ -0,0 +1,20 @@
+import type { Readable, Writable, Transform } from "stream"
+import type { UnaryFn } from "./types"
+
+/**
+ * Encryption function.
+ */
+export type EncryptFn = UnaryFn<Readable, Transform>
+
+/**
+ * Decryption function.
+ */
+export type DecryptFn = UnaryFn<Writable, Transform>
+
+/**
+ * Stream encryption module.
+ */
+export type Encrypter = {
+  encryptStream: EncryptFn
+  decryptStream: DecryptFn
+}

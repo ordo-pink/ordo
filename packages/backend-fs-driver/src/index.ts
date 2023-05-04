e27c837a3798b11df79e35d845df9714a15429d9
Sergei Orlov <sergei_orlov@ordo.pink>
2023-05-04 17:59:02 +0300

    :recycle: chore: Refactor backend-fs-utils

--- packages/backend-fs-driver/src/index.ts
+++ packages/backend-fs-driver/src/index.ts
@@ -15,21 +15,21 @@ import { OrdoDirectoryPath, OrdoFilePath, UnaryFn } from "@ordo-pink/common-type
  * @param rootDirectory Path to the directory where the data should be stored.
  */
 export const createFSDriver = (rootDirectory: string): FSDriver => {
-  const getAbsolutePath = toAbsolutePath(rootDirectory)
+  const getAbsolutePath = _toAbsolutePath(rootDirectory)
 
   return {
-    checkDirectoryExists: checkDirectoryExists(getAbsolutePath),
-    checkFileExists: checkFileExists(getAbsolutePath),
-    createDirectory: createDirectory(getAbsolutePath),
-    createFile: createFile(getAbsolutePath),
-    deleteDirectory: deleteDirectory(getAbsolutePath),
-    deleteFile: deleteFile(getAbsolutePath),
-    getDirectoryChildren: getDirectoryChildren(getAbsolutePath),
-    getFile: getFile(getAbsolutePath),
-    getFileDescriptor: getFileDescriptor(getAbsolutePath),
-    moveDirectory: moveDirectory(getAbsolutePath),
-    moveFile: moveFile(getAbsolutePath),
-    updateFile: updateFile(getAbsolutePath),
+    checkDirectoryExists: _checkDirectoryExists(getAbsolutePath),
+    checkFileExists: _checkFileExists(getAbsolutePath),
+    createDirectory: _createDirectory(getAbsolutePath),
+    createFile: _createFile(getAbsolutePath),
+    deleteDirectory: _deleteDirectory(getAbsolutePath),
+    deleteFile: _deleteFile(getAbsolutePath),
+    getDirectoryChildren: _getDirectoryChildren(getAbsolutePath),
+    getFile: _getFile(getAbsolutePath),
+    getFileDescriptor: _getFileDescriptor(getAbsolutePath),
+    moveDirectory: _moveDirectory(getAbsolutePath),
+    moveFile: _moveFile(getAbsolutePath),
+    updateFile: _updateFile(getAbsolutePath),
   }
 }
 
@@ -37,48 +37,48 @@ export const createFSDriver = (rootDirectory: string): FSDriver => {
 
 // Types ----------------------------------------------------------------------
 
-type WithAbsolutePathFn<T = unknown> = UnaryFn<ReturnType<typeof toAbsolutePath>, T>
+type WithAbsolutePathFn<T = unknown> = UnaryFn<ReturnType<typeof _toAbsolutePath>, T>
 
 // Impl -----------------------------------------------------------------------
 
 /**
  * Turns path into absolute path. Needed for the file system to work correctly.
  */
-const toAbsolutePath = (absolute: string) => (path: string) => join(absolute, path)
+const _toAbsolutePath = (absolute: string) => (path: string) => join(absolute, path)
 
-const checkDirectoryExists: WithAbsolutePathFn<FSDriver["checkDirectoryExists"]> =
+const _checkDirectoryExists: WithAbsolutePathFn<FSDriver["checkDirectoryExists"]> =
   (getAbsolutePath) => (path) =>
     promises
       .stat(getAbsolutePath(path))
       .catch(() => false)
       .then(Boolean)
 
-const checkFileExists: WithAbsolutePathFn<FSDriver["checkFileExists"]> =
+const _checkFileExists: WithAbsolutePathFn<FSDriver["checkFileExists"]> =
   (getAbsolutePath) => (path) =>
     promises
       .stat(getAbsolutePath(path))
       .catch(() => false)
       .then(Boolean)
 
-const createDirectory: WithAbsolutePathFn<FSDriver["createDirectory"]> =
+const _createDirectory: WithAbsolutePathFn<FSDriver["createDirectory"]> =
   (getAbsolutePath) => (path) =>
     promises.mkdir(getAbsolutePath(path)).then(() => path)
 
-const createFile: WithAbsolutePathFn<FSDriver["createFile"]> =
+const _createFile: WithAbsolutePathFn<FSDriver["createFile"]> =
   (getAbsolutePath) =>
   ({ path, content }) =>
     content
       ? promiseWriteStream(content, createWriteStream(getAbsolutePath(path))).then(() => path)
       : promises.writeFile(getAbsolutePath(path), "", "utf8").then(() => path)
 
-const deleteDirectory: WithAbsolutePathFn<FSDriver["deleteDirectory"]> =
+const _deleteDirectory: WithAbsolutePathFn<FSDriver["deleteDirectory"]> =
   (getAbsolutePath) => (path) =>
     promises.rm(getAbsolutePath(path), { recursive: true, force: true }).then(() => path)
 
-const deleteFile: WithAbsolutePathFn<FSDriver["deleteFile"]> = (getAbsolutePath) => (path) =>
+const _deleteFile: WithAbsolutePathFn<FSDriver["deleteFile"]> = (getAbsolutePath) => (path) =>
   promises.unlink(getAbsolutePath(path)).then(() => path)
 
-const getDirectoryChildren: WithAbsolutePathFn<FSDriver["getDirectoryChildren"]> =
+const _getDirectoryChildren: WithAbsolutePathFn<FSDriver["getDirectoryChildren"]> =
   (getAbsolutePath) => (path) =>
     promises
       .readdir(getAbsolutePath(path), { withFileTypes: true })
@@ -90,10 +90,10 @@ const getDirectoryChildren: WithAbsolutePathFn<FSDriver["getDirectoryChildren"]>
         ),
       )
 
-const getFile: WithAbsolutePathFn<FSDriver["getFile"]> = (getAbsolutePath) => (path) =>
+const _getFile: WithAbsolutePathFn<FSDriver["getFile"]> = (getAbsolutePath) => (path) =>
   Promise.resolve(createReadStream(getAbsolutePath(path)))
 
-const getFileDescriptor: WithAbsolutePathFn<FSDriver["getFileDescriptor"]> =
+const _getFileDescriptor: WithAbsolutePathFn<FSDriver["getFileDescriptor"]> =
   (getAbsolutePath) => (path) =>
     promises.stat(getAbsolutePath(path)).then(({ size, mtime }) => ({
       path,
@@ -101,17 +101,17 @@ const getFileDescriptor: WithAbsolutePathFn<FSDriver["getFileDescriptor"]> =
       updatedAt: mtime,
     }))
 
-const moveDirectory: WithAbsolutePathFn<FSDriver["moveDirectory"]> =
+const _moveDirectory: WithAbsolutePathFn<FSDriver["moveDirectory"]> =
   (getAbsolutePath) =>
   ({ oldPath, newPath }) =>
     promises.rename(getAbsolutePath(oldPath), getAbsolutePath(newPath)).then(() => newPath)
 
-const moveFile: WithAbsolutePathFn<FSDriver["moveFile"]> =
+const _moveFile: WithAbsolutePathFn<FSDriver["moveFile"]> =
   (getAbsolutePath) =>
   ({ oldPath, newPath }) =>
     promises.rename(getAbsolutePath(oldPath), getAbsolutePath(newPath)).then(() => newPath)
 
-const updateFile: WithAbsolutePathFn<FSDriver["updateFile"]> =
+const _updateFile: WithAbsolutePathFn<FSDriver["updateFile"]> =
   (getAbsolutePath) =>
   ({ path, content }) =>
     promiseWriteStream(content, createWriteStream(getAbsolutePath(path))).then(() => path)
--- packages/backend-fs-utils/src/index.ts
+++ packages/backend-fs-utils/src/index.ts
@@ -1,1 +1,30 @@
-export * from "./lib"
+import { Readable, Writable, pipeline } from "stream"
+
+/**
+ * Returns a Promise of writing to a writable stream. Resolves on completing
+ * writing with a Promise<void>. Rejects with an error if it occurs.
+ */
+export const promiseWriteStream = (readable: Readable, writable: Writable) =>
+  new Promise<void>((resolve, reject) =>
+    pipeline(readable, writable, (error) => {
+      if (error) return reject(error)
+      resolve()
+    }),
+  )
+
+/**
+ * Returns a Promise of reading a stream. Resolves with a UTF-8 string
+ * contained in the stream. Rejects with an error if it occurs.
+ */
+export const promiseReadStream = (
+  readable: Readable,
+  encoding: BufferEncoding = "utf8",
+): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const body = [] as Buffer[]
+
+    readable
+      .on("data", (chunk) => body.push(chunk))
+      .on("error", reject)
+      .on("end", () => resolve(Buffer.concat(body).toString(encoding)))
+  })

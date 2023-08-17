f2fe60736b4019ffd6b45614078031c84a7a520c
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2023-08-17 11:02:04 +0300

    :bulb: chore: Add comments in boot.sh

--- boot/boot.sh
+++ boot/boot.sh
@@ -1,30 +1,20 @@
 #!/usr/bin/env bash
 
-# Configuration
+# --- Configuration ---
 
 set -e
 
-# Variables
-
-EOL="\n"
+# --- Variables ---
 
 PLATFORM=$(uname -ms)
 
-ESBUILD_VERSION="0.18.9" # TODO: Move to init script
-BUN_VERSION="0.7.3" # TODO: Replace with bun
-TAILWIND_VERSION="v3.1.6" # TODO: Move to init script
+ESBUILD_VERSION="0.18.9"
+BUN_VERSION="0.7.3"
+TAILWIND_VERSION="v3.1.6"
 
-if [ ! -f ./.env ]; then
-  cp .env.example .env
-fi
+# --- Internal ---
 
-if [ ! -d ./opt ]; then
-  mkdir ./opt
-fi
-
-# Internal
-
-# TODO: Move to init script
+# Download Tailwind executable
 function download_tailwind {
   if [ ! -f ./opt/tailwind ]; then
     dir=$(mktemp -d)
@@ -43,6 +33,7 @@ function download_tailwind {
   fi
 }
 
+# Download Bun executable
 function download_bun {
   if [ ! -f ./opt/bun ]; then
     if ! command -v unzip >/dev/null; then
@@ -52,41 +43,34 @@ function download_bun {
     dir=$(mktemp -d)
     zip=$dir/bun-$BUN_VERSION.zip
 
-    # Download the binary executable for the current platform
     case $PLATFORM in
       'Darwin arm64') curl -fLo "$zip" "https://github.com/oven-sh/bun/releases/download/bun-v$BUN_VERSION/bun-darwin-aarch64.zip";;
       'Darwin x86_64') curl -fLo "$zip" "https://github.com/oven-sh/bun/releases/download/bun-v$BUN_VERSION/bun-darwin-x64.zip";;
-      'Linux aarch64' | 'Linux arm64') "https://github.com/oven-sh/bun/releases/download/bun-v$BUN_VERSION/bun-linux-x64.zip";;
+      'Linux arm64' | 'Linux aarch64') "https://github.com/oven-sh/bun/releases/download/bun-v$BUN_VERSION/bun-linux-x64.zip";;
       'Linux x86_64') curl -fLo "$zip" "https://github.com/oven-sh/bun/releases/download/bun-v$BUN_VERSION/bun-linux-x64-baseline.zip";;
       *) echo "Error: Unsupported platform: $PLATFORM"; exit 1;;
     esac
 
-  # Extract the binary executable to the opt directory
     unzip -o $zip -d opt &>/dev/null
     chmod +x opt/bun
     rm $zip
   fi
 }
 
-# TODO: Move to init script
-# Download the ESBuild executable
+# Download ESBuild executable
 function download_esbuild {
   if [ ! -f ./opt/esbuild ]; then
     dir=$(mktemp -d)
     tgz="$dir/esbuild-$ESBUILD_VERSION.tgz"
 
-    # Download the binary executable for the current platform
     case $PLATFORM in
       'Darwin arm64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/darwin-arm64/-/darwin-arm64-0.18.9.tgz";;
       'Darwin x86_64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/darwin-x64/-/darwin-x64-0.18.9.tgz";;
       'Linux arm64' | 'Linux aarch64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/linux-arm64/-/linux-arm64-0.18.9.tgz";;
       'Linux x86_64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/linux-x64/-/linux-x64-0.18.9.tgz";;
-      'NetBSD amd64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/netbsd-x64/-/netbsd-x64-0.18.9.tgz";;
-      'OpenBSD amd64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/openbsd-x64/-/openbsd-x64-0.18.9.tgz";;
       *) echo "Error: Unsupported platform: $PLATFORM"; exit 1;;
     esac
 
-    # Extract the binary executable to the opt directory
     tar -xzf "$tgz" -C "$dir" package/bin/esbuild
     mv "$dir/package/bin/esbuild" opt
     rm "$tgz"
@@ -98,28 +82,34 @@ function compile_init_script {
   opt/bun build boot/src/init/index.ts --compile --outfile init && mv -f init bin/init
 }
 
+# Start the init script
 function start_init_script {
   bin/init
 }
 
-# Download opt binaries
+# --- Run ---
+
+## Copy .env
+if [ ! -f ./.env ]; then
+  cp ./.env.example ./.env
+fi
+
+## Create opt directory
+if [ ! -d ./opt ]; then
+  mkdir ./opt
+fi
 
 ## Download ESBuild
-
 download_esbuild
 
 ## Download TailwindCSS
-
 download_tailwind
 
 ## Download Bun
-
 download_bun
 
-# Build init script
-
+## Build init script
 compile_init_script
 
-# Start init script
-
+## Start init script
 start_init_script

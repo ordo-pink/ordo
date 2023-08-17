485f1b4f42aee3cf69305853b65c1995119bc14e
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2023-08-17 20:01:17 +0300

    :recycle: chore: Update git hook rights

--- boot/src/init/src/_register-git-hooks.ts
+++ boot/src/init/src/_register-git-hooks.ts
@@ -6,17 +6,17 @@ export const registerGitHooks = () =>
 		.tap(startProgress)
 		.chain(() =>
 			runCommand0(`cp ./etc/git/hooks/pre-commit.sh .git/hooks/pre-commit`)
-				.chain(() => runCommand0("chmod a+x .git/hooks/pre-commit"))
+				.chain(() => runCommand0("chmod u+x .git/hooks/pre-commit"))
 				.tap(progress.inc)
 		)
 		.chain(() =>
 			runCommand0(`cp ./etc/git/hooks/commit-msg.sh .git/hooks/commit-msg`)
-				.chain(() => runCommand0("chmod a+x .git/hooks/commit-msg"))
+				.chain(() => runCommand0("chmod u+x .git/hooks/commit-msg"))
 				.tap(progress.inc)
 		)
 		.chain(() =>
 			runCommand0(`cp ./etc/git/hooks/pre-push.sh .git/hooks/pre-push`)
-				.chain(() => runCommand0("chmod a+x .git/hooks/pre-push"))
+				.chain(() => runCommand0("chmod u+x .git/hooks/pre-push"))
 				.tap(progress.inc)
 		)
 		.bimap(progress.break, progress.finish)

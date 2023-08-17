f0437933720269b9d5dc0948d5a79906353756b0
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2023-08-17 19:30:34 +0300

    :bug: fix: Provide std to git hooks

--- boot/src/init/src/_register-git-hooks.ts
+++ boot/src/init/src/_register-git-hooks.ts
@@ -6,18 +6,48 @@ export const registerGitHooks = () =>
 	Oath.empty()
 		.tap(startProgress)
 		.chain(() =>
-			runBunCommand0(`build ./etc/git/hooks/pre-commit.ts --compile --outfile pre-commit`)
-				.chain(() => runCommand0("mv -f pre-commit .git/hooks/pre-commit"))
+			runBunCommand0(`build ./etc/git/hooks/pre-commit.ts --compile --outfile pre-commit`, {
+				stdin: "inherit",
+				stderr: "inherit",
+			})
+				.chain(() =>
+					runCommand0("mv -f pre-commit .git/hooks/pre-commit", {
+						stdin: "inherit",
+						stderr: "inherit",
+					})
+				)
+				.chain(() =>
+					runCommand0("chmod a+x .git/hooks/pre-commit", { stdin: "inherit", stderr: "inherit" })
+				)
 				.tap(progress.inc)
 		)
 		.chain(() =>
-			runBunCommand0(`build ./etc/git/hooks/commit-msg.ts --compile --outfile commit-msg`)
-				.chain(() => runCommand0("mv -f commit-msg .git/hooks/commit-msg"))
+			runBunCommand0(`build ./etc/git/hooks/commit-msg.ts --compile --outfile commit-msg`, {
+				stdin: "inherit",
+				stderr: "inherit",
+			})
+				.chain(() =>
+					runCommand0("mv -f commit-msg .git/hooks/commit-msg", {
+						stdin: "inherit",
+						stderr: "inherit",
+					})
+				)
+				.chain(() =>
+					runCommand0("chmod a+x .git/hooks/commit-msg", { stdin: "inherit", stderr: "inherit" })
+				)
 				.tap(progress.inc)
 		)
 		.chain(() =>
-			runBunCommand0(`build ./etc/git/hooks/pre-push.ts --compile --outfile pre-push`)
-				.chain(() => runCommand0("mv -f pre-push .git/hooks/pre-push"))
+			runBunCommand0(`build ./etc/git/hooks/pre-push.ts --compile --outfile pre-push`, {
+				stdin: "inherit",
+				stderr: "inherit",
+			})
+				.chain(() =>
+					runCommand0("mv -f pre-push .git/hooks/pre-push", { stdin: "inherit", stderr: "inherit" })
+				)
+				.chain(() =>
+					runCommand0("chmod a+x .git/hooks/pre-push", { stdin: "inherit", stderr: "inherit" })
+				)
 				.tap(progress.inc)
 		)
 		.bimap(progress.break, progress.finish)

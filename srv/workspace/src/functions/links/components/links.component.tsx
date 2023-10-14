889ab0034efdf11bedfd3d3d26cfcc0577aca05a
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2023-10-14 17:39:22 +0300

    :sparkles: Add labels in Links function

--- srv/workspace/src/functions/links/components/links.component.tsx
+++ srv/workspace/src/functions/links/components/links.component.tsx
@@ -1,3 +1,4 @@
+import { FSID, PlainData } from "@ordo-pink/data"
 import { Either } from "@ordo-pink/either"
 import { cmd, useSharedContext } from "@ordo-pink/frontend-core"
 import { noop } from "@ordo-pink/tau"
@@ -5,24 +6,27 @@ import * as d3 from "d3"
 import { useEffect, useLayoutEffect, useRef, useState } from "react"
 
 export default function LinksComponent() {
-	const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
 	const wrapperRef = useRef<SVGSVGElement>(null)
 	const { commands, data } = useSharedContext()
-
-	useLayoutEffect(() => {
-		Either.fromNullable(wrapperRef.current)
-			.map(div => div.getBoundingClientRect())
-			.map(({ width, height }) => ({ width: Math.round(width), height: Math.round(height) }))
-			.fold(noop, setDimensions)
-	}, [wrapperRef.current])
 
 	useEffect(() => {
 		if (!data) return
 
-		const nodes = data.map(data => ({ id: data.fsid, data }))
-		const links = data.flatMap(item =>
-			item.children.map(child => ({ source: item.fsid, target: child })),
-		)
+		const nodes = [] as { id: string; data?: PlainData }[]
+		const links = [] as { target: string; source: string; type: "child" | "label" | "link" }[]
+
+		data.forEach(item => {
+			nodes.push({ id: item.fsid, data: item })
+
+			if (item.parent) links.push({ source: item.fsid, target: item.parent, type: "child" })
+
+			if (item.labels.length > 0) {
+				item.labels.forEach(id => {
+					if (!nodes.some(node => node.id === id)) nodes.push({ id })
+					links.push({ source: item.fsid, target: id, type: "label" })
+				})
+			}
+		})
 
 		const drag = (simulation: d3.Simulation<any, any>) => {
 			return d3
@@ -53,13 +57,7 @@ export default function LinksComponent() {
 					.distance(50)
 					.strength(1),
 			)
-			.force("charge", d3.forceManyBody().theta(1).strength(-300))
-			.force(
-				"collision",
-				d3.forceCollide().radius(function (d) {
-					return d.radius
-				}),
-			)
+			.force("charge", d3.forceManyBody().theta(1).strength(-2500))
 			.force("x", d3.forceX())
 			.force("y", d3.forceY())
 
@@ -79,28 +77,32 @@ export default function LinksComponent() {
 				const payload = data.find(item => item.fsid === id)
 				commands.emit<cmd.ctxMenu.show>("context-menu.show", { event, payload })
 			})
-			.call(d3.zoom().on("zoom", e => svg.attr("transform", e.transform)) as any)
+			.call(
+				d3.zoom().on("zoom", e => {
+					svg.attr("transform", e.transform)
+				}),
+			)
 
 		// Append links.
 		const link = svg
 			.append("g")
-			.attr("stroke", "#999")
-			.attr("stroke-opacity", 0.6)
+			.attr("stroke-opacity", 0.2)
 			.selectAll("line")
 			.data(links)
 			.join("line")
+			.attr("stroke", l => (l.type === "child" ? "#737373" : "#a855f7"))
 
 		// Append nodes.
 		const node = svg
 			.append("g")
-			.attr("fill", "#fff")
 			.selectAll("circle")
 			.data(nodes)
 			.join("circle")
 			.attr("r", 10)
+			.attr("class", d => (d.data ? "fill-neutral-100 cursor-grab" : "fill-purple-500 cursor-grab"))
 			.call(drag(simulation))
 
-		node.append("title").text(d => d.data.name)
+		node.append("title").text(d => d.data?.name ?? d.id)
 
 		var label = svg
 			.append("g")
@@ -108,44 +110,20 @@ export default function LinksComponent() {
 			.data(nodes)
 			.enter()
 			.append("text")
-			.text(d => d.data.name)
+			.text(d => d.data?.name ?? d.id)
 			.attr("x", 20)
 			.attr("class", "fill-neutral-500 cursor-grab text-xs")
 			.call(drag(simulation))
 
-		// const texts = node
-		// 	.selectAll(null)
-		// 	.append("text")
-		// 	.attr("x", 0)
-		// 	.attr("y", 0)
-		// 	.attr("text-anchor", "middle")
-		// 	.attr("stroke-width", 1)
-		// 	.attr("fill", "#333")
-		// 	.attr("style", "font-size: 25px")
-		// 	.text(d => d.data.name)
-
 		simulation.on("tick", () => {
 			link
-				.attr("x1", d => d.source.x)
+				.attr("x1", d => d.source.x - 12)
 				.attr("y1", d => d.source.y)
-				.attr("x2", d => d.target.x)
+				.attr("x2", d => d.target.x - 12)
 				.attr("y2", d => d.target.y)
 
-			node
-				.attr("cx", function (d) {
-					return d.x + 5
-				})
-				.attr("cy", function (d) {
-					return d.y - 3
-				})
-
-			label
-				.attr("x", function (d) {
-					return d.x
-				})
-				.attr("y", function (d) {
-					return d.y
-				})
+			node.attr("cx", d => d.x - 14).attr("cy", d => d.y - 3)
+			label.attr("x", d => d.x).attr("y", d => d.y)
 		})
 
 		return () => {

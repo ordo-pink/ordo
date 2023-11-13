558737a4e14e372f1c903764c4c49c1058bb890a
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2023-11-13 22:19:33 +0300

    :sparkles: Update graph styles

--- srv/workspace/src/functions/links/components/2d-links.component.tsx
+++ srv/workspace/src/functions/links/components/2d-links.component.tsx
@@ -1,11 +1,29 @@
 // SPDX-FileCopyrightText: Copyright 2023, 谢尔盖||↓ and the Ordo.pink contributors
 // SPDX-License-Identifier: MIT
 
+import { useIsDarkTheme } from "$hooks/use-is-dark-theme.hook"
 import { useWorkspaceWidth } from "$hooks/use-workspace-width.hook"
 import { PlainData } from "@ordo-pink/data"
 import { cmd, useSharedContext } from "@ordo-pink/frontend-core"
 import { useRef } from "react"
 import { ForceGraph2D } from "react-force-graph"
+import "./2d-links.component.css"
+
+const DarkThemeColors = {
+	LABEL_LINK: "#8b5cf6",
+	LABEL_NODE: "#8b5cf6",
+	CHILD_LINK: "#e5e5e5",
+	CHILD_NODE: "#e5e5e5",
+	LINK: "#ea580c",
+}
+
+const LightThemeColors = {
+	LABEL_LINK: "#8b5cf6",
+	LABEL_NODE: "#8b5cf6",
+	CHILD_LINK: "#525252",
+	CHILD_NODE: "#525252",
+	LINK: "#ea580c",
+}
 
 type P = {
 	nodes: { id: string; data?: PlainData }[]
@@ -15,6 +33,9 @@ export default function Links2D({ nodes, links }: P) {
 	const { commands } = useSharedContext()
 	const { workspaceWidth } = useWorkspaceWidth()
 	const ref = useRef<any>()
+	const isDarkTheme = useIsDarkTheme()
+
+	const colors = isDarkTheme ? DarkThemeColors : LightThemeColors
 
 	return (
 		<ForceGraph2D
@@ -23,9 +44,13 @@ export default function Links2D({ nodes, links }: P) {
 			graphData={{ nodes, links }}
 			backgroundColor="rgba(0,0,0,0)"
 			nodeRelSize={5}
-			nodeColor={node => (node.data ? "#e5e5e5" : "#8b5cf6")}
+			nodeColor={node => (node.data ? colors.CHILD_NODE : colors.LABEL_NODE)}
 			linkColor={link =>
-				link.type === "label" ? "#8b5cf6" : link.type === "link" ? "#ea580c" : "#e5e5e5"
+				link.type === "label"
+					? colors.LABEL_LINK
+					: link.type === "link"
+					? colors.LINK
+					: colors.CHILD_LINK
 			}
 			linkWidth={0.3}
 			nodeLabel={node => (!node.data ? `#${node.id}` : node.data.name)}
---
+++ srv/workspace/src/hooks/use-is-dark-theme.hook.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+export const useIsDarkTheme = () => {
+	const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+	useEffect(() => {
+		const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
+
+		const handleDarkModeChange = (event: MediaQueryListEvent) => setIsDarkTheme(event.matches)
+
+		darkModeMediaQuery.addEventListener("change", handleDarkModeChange)
+
+		return () => darkModeMediaQuery.removeEventListener("change", handleDarkModeChange)
+	}, [])
+
+	return isDarkTheme
+}

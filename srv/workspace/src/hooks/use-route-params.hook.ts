c2b78fb5fa592c00e3671b0a85af8e74d9b0169e
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2024-01-15 19:52:44 +0300

    :bug: fix: Decode URI component inside useRouteParams hook directly

--- srv/workspace/src/hooks/use-route-params.hook.ts
+++ srv/workspace/src/hooks/use-route-params.hook.ts
@@ -1,12 +1,23 @@
 // SPDX-FileCopyrightText: Copyright 2023, 谢尔盖||↓ and the Ordo.pink contributors
 // SPDX-License-Identifier: MIT
 
+import { Either } from "@ordo-pink/either"
 import { useSharedContext } from "@ordo-pink/frontend-core"
+import { keysOf } from "@ordo-pink/tau"
 
 export const useRouteParams = <
 	ExpectedRouteParams extends Record<string, string> = Record<string, string>,
 >(): Partial<ExpectedRouteParams> => {
 	const { route } = useSharedContext()
 
-	return (route?.params ?? {}) as Partial<ExpectedRouteParams>
+	return Either.fromNullable(route)
+		.chain(route => Either.fromNullable(route.params))
+		.fold(
+			() => ({}),
+			params =>
+				keysOf(params).reduce(
+					(acc, key) => ({ ...acc, [key]: decodeURIComponent(params[key]) }),
+					{},
+				),
+		)
 }

8f878a0786310ee40b30ed10a975ccdec0f1f051
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2024-12-07 01:06:06 +0300

    :truck: chore: Remove redundant files

--- lib/cmd-init/src/cmd-init.impl.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-FileCopyrightText: Copyright 2024, 谢尔盖 ||↓ and the Ordo.pink contributors
- * SPDX-License-Identifier: Unlicense
- */
-
-import type { TCmdInit } from "./cmd-init.types"
-
-export const cmd_init: TCmdInit = "cmd-init"
--- lib/cmd-init/src/cmd-init.types.ts
+++
@@ -1,6 +0,0 @@
-/*
- * SPDX-FileCopyrightText: Copyright 2024, 谢尔盖 ||↓ and the Ordo.pink contributors
- * SPDX-License-Identifier: Unlicense
- */
-
-export type TCmdInit = "cmd-init"

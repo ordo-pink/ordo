ae44d5d583a46f9c09d81396565653a81d86445d
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-01-03 00:24:25 +0300

    :hammer: build: fix build command

--- lib/maoka-components/src/common/link.component.ts
+++ lib/maoka-components/src/common/link.component.ts
@@ -19,36 +19,36 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Maoka, TMaokaChildren } from "@ordo-pink/maoka"
-import { MaokaJabs } from "@ordo-pink/maoka-jabs"
-import { MaokaOrdo } from "@ordo-pink/maoka-ordo-jabs"
-import { is_string } from "@ordo-pink/tau"
+// import { Maoka, TMaokaChildren } from "@ordo-pink/maoka"
+// import { MaokaJabs } from "@ordo-pink/maoka-jabs"
+// import { MaokaOrdo } from "@ordo-pink/maoka-ordo-jabs"
+// import { is_string } from "@ordo-pink/tau"
 
-type P = { href: string; children?: TMaokaChildren; custom_class?: string; show_visited?: boolean }
-export const Link = ({ href, children, custom_class, show_visited }: P) =>
-	Maoka.create("a", ({ use }) => {
-		const { emit } = use(MaokaOrdo.Jabs.Commands.get)
+// type P = { href: string; children?: TMaokaChildren; custom_class?: string; show_visited?: boolean }
+// export const Link = ({ href, children, custom_class, show_visited }: P) =>
+// 	Maoka.create("a", ({ use }) => {
+// 		const { emit } = use(MaokaOrdo.Jabs.Commands.get)
 
-		use(MaokaJabs.listen("onclick", click_listener(emit, href)))
-		use(MaokaJabs.set_attribute("href", href))
-		use(MaokaJabs.set_class(default_class))
+// 		use(MaokaJabs.listen("onclick", click_listener(emit, href)))
+// 		use(MaokaJabs.set_attribute("href", href))
+// 		use(MaokaJabs.set_class(default_class))
 
-		if (custom_class) use(MaokaJabs.add_class(custom_class))
-		if (!show_visited) use(MaokaJabs.add_class(ignore_history_highlighting_class))
-		if (is_string(children)) use(MaokaJabs.set_attribute("title", children))
+// 		if (custom_class) use(MaokaJabs.add_class(custom_class))
+// 		if (!show_visited) use(MaokaJabs.add_class(ignore_history_highlighting_class))
+// 		if (is_string(children)) use(MaokaJabs.set_attribute("title", children))
 
-		return () => children
-	})
+// 		return () => children
+// 	})
 
-// --- Internal ---
+// // --- Internal ---
 
-const click_listener = (emit: Ordo.Command.Commands["emit"], href: string) => (event: MouseEvent) => {
-	event.preventDefault()
-	event.stopPropagation()
+// const click_listener = (emit: Ordo.Command.Commands["emit"], href: string) => (event: MouseEvent) => {
+// 	event.preventDefault()
+// 	event.stopPropagation()
 
-	emit("cmd.application.router.navigate", href)
-}
+// 	emit("cmd.application.router.navigate", href)
+// }
 
-const ignore_history_highlighting_class = "text-inherit visited:text-inherit"
+// const ignore_history_highlighting_class = "text-inherit visited:text-inherit"
 
-const default_class = "underline cursor-pointer decoration-neutral-500/50 decoration-1 underline-offset-2"
+// const default_class = "underline cursor-pointer decoration-neutral-500/50 decoration-1 underline-offset-2"

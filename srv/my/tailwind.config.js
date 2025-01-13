7e31992e38e240bfd2deca8d857154cc809529d1
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-01-13 22:38:00 +0300

    :heavy_minus_sign: remove redundant dependencies

--- srv/my/tailwind.config.js
+++ srv/my/tailwind.config.js
@@ -1,7 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["../../lib/**/*.{css,ts,tsx}", "./index.html", "./src/**/*.{css,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-}
+export default { content: ["../../lib/**/*.{css,ts}", "./index.html", "./src/**/*.{css,ts}"], theme: { extend: {} } }

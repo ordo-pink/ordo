d354b7513eb2c06fa7e79a77100adc1da746e04a
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-03-10 21:36:19 +0300

    Temporarily hide sign up button

--- lib/frontend-app/src/sections/welcome/pages/landing.page.ts
+++ lib/frontend-app/src/sections/welcome/pages/landing.page.ts
@@ -112,18 +112,18 @@ export default Maoka.create("main", ({ use }) => {
 								CallToActionCard(() => () => [
 									CallToActionBetaLogo(`"${t_beta_started}"`),
 									ActionsContainer(() => () => [
-										Button.Neutral({
+										Button.Primary({
 											text: t_try_now,
 											hotkey: "mod+enter",
 											on_click: handle_try_click,
 											hotkey_options: { prevent_in_inputs: true },
 										}),
-										Button.Primary({
-											text: t_sign_up,
-											hotkey: "mod+j",
-											on_click: handle_sign_up_click,
-											hotkey_options: { prevent_in_inputs: true },
-										}),
+										// Button.Primary({
+										// 	text: t_sign_up,
+										// 	hotkey: "mod+j",
+										// 	on_click: handle_sign_up_click,
+										// 	hotkey_options: { prevent_in_inputs: true },
+										// }),
 									]),
 								]),
 						),

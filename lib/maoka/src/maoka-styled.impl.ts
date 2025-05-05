6cddf049fcd31ff0178e33947a102a8030d39b1a
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-05-05 14:11:40 +0300

    add missing spdx records

--- lib/maoka/src/maoka-styled.impl.ts
+++ lib/maoka/src/maoka-styled.impl.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2025, 谢尔盖 ||↓ and the Ordo.pink contributors
+ * SPDX-License-Identifier: Unlicense
+ */
+
 import { HTML_TAGS } from "./html-tags"
 import { Maoka } from "./maoka.types"
 import { maoka } from "./maoka.impl"

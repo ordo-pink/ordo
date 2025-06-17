dd36f0eac56e94e76e53902fd8ac07dd0298dfcf
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-06-17 13:28:09 +0300

    modal: move jab to index.ts

--- lib/frontend-app/src/modal/index.ts
+++ lib/frontend-app/src/modal/index.ts
@@ -19,6 +19,36 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { Maoka, maoka_dom } from "@ordo-pink/maoka"
+import { context } from "@ordo-pink/sdk-maoka"
+
+import { close_modal } from "./components/close-modal.component"
+import { modal } from "./components/modal.component"
+import { modal$ } from "./modal.state"
+import { overlay } from "./components/overlay.component"
+
 import "./modal.style.css"
 
-export * from "./modal.jab"
+export const create_modal_jab: Maoka.Jab<() => Maoka.Component> = ({ use }) => {
+	use(internal.track_prey_jab)
+
+	return () => overlay(() => [modal(), close_modal()])
+}
+
+namespace internal {
+	export const track_prey_jab: Maoka.Jab = ({ use }) => {
+		const { hunter } = use(context.consume)
+
+		const handle_mount = () => {
+			const release_show = hunter.track("modal.show", params => modal$.update("instance", () => params))
+			const release_hide = hunter.track("modal.hide", () => modal$.update("instance", () => void 0))
+
+			return () => {
+				release_hide()
+				release_show()
+			}
+		}
+
+		use(maoka_dom.jabs.onmount(handle_mount))
+	}
+}

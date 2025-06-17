200f6c74dc0131900690b7ef52361d8c6a77fe86
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-06-17 13:26:55 +0300

    rotor: add support for opening external links

--- lib/frontend-app/src/rotor/index.ts
+++ lib/frontend-app/src/rotor/index.ts
@@ -8,10 +8,12 @@ export const create_rotor_jab: (hunter: ClientSDK.Hunter) => Maoka.Jab<RoutaryBr
 	({ use }) => {
 		const rotor = create_routary_browser(window)
 
-		// TODO External routes
+		const open = window.open
+		window.open = undefined as any
 
 		const handle_onmount = () => {
 			const release_set_hash = hunter.track("router.set_hash", rotor.set_hash)
+			const release_set_href = hunter.track("router.set_href", href => void open(href, "_blank")?.focus())
 			const release_set_pathname = hunter.track("router.set_pathname", rotor.set_pathname)
 			const release_set_search = hunter.track("router.set_search", params =>
 				core_sdk.validations.is_string(params) ? rotor.set_search(params) : rotor.set_search_params(params),
@@ -19,6 +21,7 @@ export const create_rotor_jab: (hunter: ClientSDK.Hunter) => Maoka.Jab<RoutaryBr
 
 			return () => {
 				release_set_hash()
+				release_set_href()
 				release_set_pathname()
 				release_set_search()
 

a07b0ebd34b43d05611ebc8b53da17bf52dc673e
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-07-09 14:59:44 +0300

    frontend-app: clean up

--- lib/frontend-app/src/user/components/other-user-workspace.component.ts
+++
@@ -1,5 +0,0 @@
-import { maoka } from "@ordo-pink/maoka"
-
-export const other_user_workspace = maoka.create("div", () => {
-	return () => "HELLO OTHER USER"
-})
--- lib/frontend-app/src/user/user.state.ts
+++
@@ -1,5 +0,0 @@
-import { create_zags } from "@ordo-pink/zags"
-
-import type { AuthenticatingUser } from "./user.types"
-
-export const authenticating_user$ = create_zags<AuthenticatingUser.State>({ code: "", email: "" })

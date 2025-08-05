7af6d406fb1365eccfa0d174d276f1d550a17ad0
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-08-06 00:59:29 +0300

    add missing spdx records

--- lib/oss-aist/src/aist.constants.ts
+++ lib/oss-aist/src/aist.constants.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2025, 谢尔盖 ||↓ and the Ordo.pink contributors
+ * SPDX-License-Identifier: Unlicense
+ */
+
 import type * as Lib from "./aist.types"
 
 export const DEFAULT_STATE: Lib.State = { hash: "", pathname: "/", search: "" }

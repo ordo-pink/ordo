c41dd2d2c91b4c2b825f834a15c353678a7fec1f
谢尔盖 ||↓ <sergei_orlov@ordo.pink>
2025-11-22 22:01:27 +0300

    only add toggle sidebar to cp if current activity has a sidebar

--- lib/client-app/src/components/main/main.component.ts
+++ lib/client-app/src/components/main/main.component.ts
@@ -37,82 +37,55 @@ export const sidebar_toggle = maoka.create("button", ({ use }) => {
 	}
 })
 
-// TODO Close sidebar if there isn't one
 export const sidebar = maoka.create("aside", ({ use, node }) => {
 	const { query, hunter } = use(ordo_client_maoka.context.consume)
-	const get_sidebar = use(ordo_client_maoka.jabs.marry$(sidebar$))
-	const get_current_activity = use(ordo_client_maoka.jabs.cheat$(query, "activities.current"))
 	const is_mobile = use(ordo_client_maoka.jabs.is_mobile)
-
-	use(maoka_dom.jabs.onmount(() => handle_mount()))
-	use(ordo_client_maoka.jabs.listen("onclick", () => handle_click()))
+	const get_sidebar$ = use(ordo_client_maoka.jabs.marry$(sidebar$))
+	const get_current_activity$ = use(ordo_client_maoka.jabs.cheat$(query, "activities.current"))
 
 	const handle_click = () => is_mobile && hunter.shoot("sidebar.hide")
 
-	const handle_mount = () => {
-		const release_disable = hunter.track("sidebar.disable", () => sidebar$.update("enabled", () => false))
-		const release_enable = hunter.track("sidebar.enable", () => sidebar$.update("enabled", () => true))
-		const release_hide = hunter.track("sidebar.hide", () => sidebar$.update("visible", () => false))
-		const release_show = hunter.track("sidebar.show", () => sidebar$.update("visible", () => true))
-		const release_toggle = hunter.track(
-			"sidebar.toggle",
-			() => void (sidebar$.select("enabled") && sidebar$.update("visible", prev => !prev)),
-		)
+	const handle_disable: OrdoClient.Command.GunFor<"sidebar.disable"> = () => sidebar$.update("enabled", () => false)
+	const handle_enable: OrdoClient.Command.GunFor<"sidebar.enable"> = () => sidebar$.update("enabled", () => true)
+	const handle_hide: OrdoClient.Command.GunFor<"sidebar.hide"> = () => sidebar$.update("visible", () => false)
+	const handle_show: OrdoClient.Command.GunFor<"sidebar.show"> = () => sidebar$.update("visible", () => true)
+	const handle_toggle: OrdoClient.Command.GunFor<"sidebar.toggle"> = () =>
+		void (sidebar$.select("enabled") && sidebar$.update("visible", prev => !prev))
 
-		hunter.shoot("command_palette.add", {
-			id: "sidebar.toggle",
-			value: () => hunter.shoot("sidebar.toggle"),
-			hotkey: "mod+b", // TODO Replace with meta+b
-			type: ORDO_CLIENT.COMMAND_PALETTE.ITEM_TYPE.COMMON_ACTION,
-			readable_name: "sidebar_commands_toggle_title",
-			description: "sidebar_commands_toggle_description",
-			render_icon: span => maoka_dom.render(span, bs_layout_sidebar_inset_reverse(), () => crypto.randomUUID()),
-		})
-
-		hunter.shoot("i18n.add_translations", {
-			locale: "en",
-			values: {
-				sidebar_commands_hide_title: "Hide Sidebar",
-				sidebar_commands_hide_description: "Hide sidebar away from the sight!",
-				sidebar_commands_show_title: "Show Sidebar",
-				sidebar_commands_show_description: "Show the annoying thing on the right. Why isn't it on the left tho?",
-				sidebar_commands_toggle_title: "Toggle Sidebar",
-				sidebar_commands_toggle_description: "Show or hide Sidebar depending on whether it is, well, hidden or shown.",
-			},
-		})
-
-		return () => {
-			release_disable()
-			release_enable()
-			release_hide()
-			release_show()
-			release_toggle()
-
-			hunter.shoot("command_palette.remove", "sidebar.toggle")
-			hunter.shoot("i18n.remove_translations", [
-				"sidebar_commands_hide_description",
-				"sidebar_commands_hide_title",
-				"sidebar_commands_show_description",
-				"sidebar_commands_show_title",
-				"sidebar_commands_toggle_description",
-				"sidebar_commands_toggle_title",
-			])
-		}
-	}
+	use(ordo_client_maoka.jabs.listen("onclick", () => handle_click()))
+	use(ordo_client_maoka.jabs.handle_command("sidebar.disable", handle_disable))
+	use(ordo_client_maoka.jabs.handle_command("sidebar.enable", handle_enable))
+	use(ordo_client_maoka.jabs.handle_command("sidebar.hide", handle_hide))
+	use(ordo_client_maoka.jabs.handle_command("sidebar.show", handle_show))
+	use(ordo_client_maoka.jabs.handle_command("sidebar.toggle", handle_toggle))
+	use(ordo_client_maoka.jabs.add_translations("en", en_values))
 
 	return () => {
-		const { visible, enabled } = get_sidebar()
+		const { visible, enabled } = get_sidebar$()
 
-		const current_activity = get_current_activity()
+		const current_activity = get_current_activity$()
 
 		if (current_activity && current_activity.render_sidebar) hunter.shoot("sidebar.enable")
 		else hunter.shoot("sidebar.disable")
 
 		if (!visible || !enabled || !maoka_dom.node_guard(node) || !node.value.parentElement) {
+			// TODO Add or remove cp sidebar toggler
 			use(ordo_client_maoka.jabs.add_class("hidden"))
+			hunter.shoot("command_palette.remove", "sidebar.toggle")
+
 			return null
 		} else {
 			use(ordo_client_maoka.jabs.remove_class("hidden"))
+			hunter.shoot("command_palette.add", {
+				id: "sidebar.toggle",
+				readable_name: "sidebar_commands_toggle_title",
+				value: () => hunter.shoot("sidebar.toggle"),
+				hotkey: "mod+b",
+				type: ORDO_CLIENT.COMMAND_PALETTE.ITEM_TYPE.COMMON_ACTION,
+				description: "sidebar_commands_toggle_description",
+				render_icon: span => maoka_dom.render(span, bs_layout_sidebar_inset_reverse(), () => crypto.randomUUID()),
+			})
+
 			return sidebar_render_picker()
 		}
 	}
@@ -180,3 +153,12 @@ const sidebar$ = zags.create<{ enabled: boolean; visible: boolean }>({
 	enabled: false,
 	visible: window.innerWidth >= ORDO_CLIENT.SM_SCREEN_BREAKPOINT,
 })
+
+const en_values = {
+	sidebar_commands_hide_title: "Hide Sidebar",
+	sidebar_commands_hide_description: "Hide sidebar away from the sight!",
+	sidebar_commands_show_title: "Show Sidebar",
+	sidebar_commands_show_description: "Show the annoying thing on the right. Why isn't it on the left tho?",
+	sidebar_commands_toggle_title: "Toggle Sidebar",
+	sidebar_commands_toggle_description: "Show or hide Sidebar depending on whether it is, well, hidden or shown.",
+}
